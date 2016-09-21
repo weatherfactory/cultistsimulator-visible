0145ff11c9cda49d38cdb73ff953126718933037
alexiskennedy <alexis@weatherfactory.biz>
2016-09-21 10:56:43 +0100

    Initial commit

---
+++ Assets/UIManager_Script.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class UIManager_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Starting");
+	}
+	
+	// Update is called once per frame
+	//void Update () {
+	
+	//}
+	public void NewGame()
+	{
+		Debug.Log ("New");
+		SceneManager.LoadScene ("board");
+	}
+	public void ExitGame()
+	{
+		Debug.Log ("Exiting");
+		Application.Quit ();
+	}
+}
