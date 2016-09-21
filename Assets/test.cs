b8f83e1c6d61c3490b244f6e240eaa3c25511b1c
alexiskennedy <alexis@weatherfactory.biz>
2016-09-21 13:35:50 +0100

    Some nonsense test code

---
+++ Assets/test.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+    private int counter;
+    // Use this for initialization
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        { 
+            counter++;
+        gameObject.GetComponent<Text>().text += ("\n" + counter.ToString());
+        }
+    }
+}
