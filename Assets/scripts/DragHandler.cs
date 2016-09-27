0d08b77c4877607d9f8423eb2e7d7ce53bc0f94c
alexiskennedy <alexis@weatherfactory.biz>
2016-09-27 17:54:31 +0100

    Drag and drop distinguishes between verbs and elements

--- Assets/scripts/DragHandler.cs
+++ Assets/scripts/DragHandler.cs
@@ -2,17 +2,19 @@
 using System.Collections;
 using UnityEngine.EventSystems;
 
-public class TestDragHandler : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDragHandler
+public class DragHandler : MonoBehaviour,IDragHandler,IBeginDragHandler,IEndDragHandler
 {
     private Vector3 startPosition;
     public static GameObject itemBeingDragged;
-
+    private Transform startParent;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("began drag");
         itemBeingDragged = gameObject;
         startPosition = transform.position;
+        startParent = transform.parent;
+        if (GetComponent<CanvasGroup>() != null)
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -24,7 +26,9 @@ public class TestDragHandler : MonoBehaviour,IDragHandler,IBeginDragHandler,IEnd
     public void OnEndDrag(PointerEventData eventData)
     {
         itemBeingDragged = null;
+        if(transform.parent==startParent)
         transform.position = startPosition;
-        
+        if (GetComponent<CanvasGroup>() != null)
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
 }
---
+++ Assets/scripts/SlotReceiveVerb.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class SlotReceiveVerb : MonoBehaviour, IDropHandler {
+
+
+    public GameObject itemInSlot
+    {
+        get
+        {
+            if (transform.childCount > 0)
+                return transform.GetChild(0).gameObject;
+            else
+                return null;
+
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (!itemInSlot && DragHandler.itemBeingDragged.tag=="Verb")
+            DragHandler.itemBeingDragged.transform.SetParent(transform);
+    }
+}
