9e3f460d839ead66c873c98391bc3a3d23a8cd53
Alexis Kennedy <ak@underspace.net>
2016-10-28 19:36:34 +0100

    Resource/Workspace counts; tests supporting

---
+++ Assets/Editor/Tests/CharacterTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+[TestFixture]
+public class CharacterTests
+{
+    private const string EL1 = "el1";
+
+    [Test]
+    public void AddingElement_AffectsBasicCount()
+    {
+        Character c = new Character();
+        Assert.AreEqual(0, c.GetCurrentElementQuantity(EL1));
+        c.ModifyElementQuantity(EL1, 1);
+        Assert.AreEqual(1, c.GetCurrentElementQuantity(EL1));
+    }
+
+    [Test]
+    public void AddingElement_AffectsResourcesCount()
+    {
+        Character c = new Character();
+        Assert.AreEqual(0, c.GetCurrentElementQuantityInResources(EL1));
+        c.ModifyElementQuantity(EL1, 1);
+        Assert.AreEqual(1, c.GetCurrentElementQuantityInResources(EL1));
+    }
+
+    [Test]
+    public void AddingElement_DoesNotAffectWorkspaceCount()
+    {
+        Character c = new Character();
+        Assert.AreEqual(0, c.GetCurrentElementQuantityInWorkspace(EL1));
+        c.ModifyElementQuantity(EL1, 1);
+        Assert.AreEqual(0, c.GetCurrentElementQuantityInWorkspace(EL1));
+    }
+
+    [Test]
+    public void ElementToWorkspace_IsReflectedInCounts()
+    {
+        Character c = new Character();
+        c.ModifyElementQuantity(EL1,10);
+        c.ElementToWorkspace(EL1, 3);
+        Assert.AreEqual(3, c.GetCurrentElementQuantityInWorkspace(EL1));
+        Assert.AreEqual(7, c.GetCurrentElementQuantityInResources(EL1));
+        Assert.AreEqual(10, c.GetCurrentElementQuantity(EL1));
+        c.ElementFromWorkspace(EL1, 1);
+        Assert.AreEqual(8, c.GetCurrentElementQuantityInResources(EL1));
+        Assert.AreEqual(2, c.GetCurrentElementQuantityInWorkspace(EL1));
+        Assert.AreEqual(10, c.GetCurrentElementQuantity(EL1));
+
+    }
+}
+
--- Assets/scripts/Entities/Character.cs
+++ Assets/scripts/Entities/Character.cs
@@ -7,7 +7,17 @@ using JetBrains.Annotations;
 
 public class Character:IElementsContainer
     {
-        public string Title
+    private readonly Dictionary<string, int> _elements;
+    private readonly Dictionary<string, int> _elementsInWorkspace;
+    private readonly List<IElementQuantityDisplay> _elementsDisplaySubscribers;
+    private readonly List<ICharacterInfoSubscriber> _detailsSubscribers;
+    private string _title;
+    private string _firstName;
+    private string _lastName;
+    public CharacterState State { get; set; }
+    private string _endingTriggeredId;
+
+    public string Title
         {
             get { return _title; }
             set
@@ -41,19 +51,13 @@ public class Character:IElementsContainer
     public string EndingTriggeredId
     { get { return _endingTriggeredId; } }
 
-    private readonly Dictionary<string, int> _elements;
-        private readonly List<IElementQuantityDisplay> _elementsDisplaySubscribers;
-        private readonly List<ICharacterInfoSubscriber> _detailsSubscribers;
-        private string _title;
-        private string _firstName;
-        private string _lastName;
-        public CharacterState State { get; set; }
-        private string _endingTriggeredId;
+
 
 
         public Character()
         {
             _elements=new Dictionary<string, int>();
+        _elementsInWorkspace=new Dictionary<string, int>();
         _elementsDisplaySubscribers=new List<IElementQuantityDisplay>();
         _detailsSubscribers=new List<ICharacterInfoSubscriber>();
             State = CharacterState.Viable;
@@ -94,6 +98,39 @@ public class Character:IElementsContainer
             else
                 _elements[elementId] = _elements[elementId] + quantity;
             NotifySubscribersOfElementQuantityChange(elementId, _elements[elementId]);
+        }
+
+   public bool ElementToWorkspace(string elementId, int plusQuantity)
+   {
+        if(!_elementsInWorkspace.ContainsKey(elementId))
+            _elementsInWorkspace.Add(elementId,plusQuantity);
+        else
+           _elementsInWorkspace[elementId] += plusQuantity;
+       return true;
+   }
+
+    public bool ElementFromWorkspace(string elementId, int minusQuantity)
+    {
+        if (!_elementsInWorkspace.ContainsKey(elementId))
+            _elementsInWorkspace.Add(elementId, -minusQuantity);
+        else
+            _elementsInWorkspace[elementId] -= minusQuantity;
+        return true;
+    }
+
+    public int GetCurrentElementQuantityInWorkspace(string elementId)
+    {
+        return _elementsInWorkspace.ContainsKey(elementId) ? _elementsInWorkspace[elementId] : 0;
+    }
+
+        public int GetCurrentElementQuantityInResources(string elementId)
+        {
+            var inResources = _elements.ContainsKey(elementId) ? _elements[elementId] : 0;
+
+            if (_elementsInWorkspace.ContainsKey(elementId))
+                inResources -= _elementsInWorkspace[elementId];
+
+            return inResources;
         }
 
         public int GetCurrentElementQuantity(string elementId)
