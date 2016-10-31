3841387f26aa007691a1f730b71c436c7eb34bed
alexiskennedy <alexis@weatherfactory.biz>
2016-10-31 14:42:07 +0000

    missed a commit for RecipeSituation refactor!

--- Assets/scripts/Recipes/TimerPanel.cs
+++ Assets/scripts/Recipes/TimerPanel.cs
@@ -13,32 +13,6 @@ public class TimerPanel : BoardMonoBehaviour,IRecipeSituationSubscriber
     public RecipeSituation RecipeSituation;
 
 
-    //public void StartTimer(Recipe r,float? timeRemaining)
-    //{
-    //    if (RecipeSituation!=null)
-    //        throw new ApplicationException("already running a recipe (" + RecipeSituation.Recipe.Id + ")");
-    //    RecipeSituation = new RecipeSituation(r, timeRemaining);
-    //    UpdateTimerText();
-
-    //}
-
-    //public RecipeTimerState DoHeartbeat(Character c)
-    //{
-        
-    //    RecipeTimerState state=RecipeSituation.DoHeartbeat(c, ContentRepository.Instance.RecipeCompendium);
-    //    if (state==RecipeTimerState.Complete)     
-    //        BM.ExileToLimboThenDestroy(gameObject);
-    //    else
-    //    {
-    //        float fillAmount = RecipeSituation.TimeRemaining / RecipeSituation.Recipe.Warmup;
-    //        imgTimer.fillAmount = fillAmount;
-    //        UpdateTimerText();
-    //    }
-    //    return state;
-    //}
-
-
-
     public void ReceiveSituationUpdate(Recipe recipe, RecipeTimerState state, float timeRemaining)
     {
         if (state == RecipeTimerState.Extinct)
