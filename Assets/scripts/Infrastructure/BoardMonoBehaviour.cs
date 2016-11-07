bbf737cebf51cfc3d980064109cd74c2fa06666b
alexiskennedy <alexis@weatherfactory.biz>
2016-11-07 11:36:05 +0000

    Added comment to BoardMonoBehaviour

--- Assets/scripts/Infrastructure/BoardMonoBehaviour.cs
+++ Assets/scripts/Infrastructure/BoardMonoBehaviour.cs
@@ -4,7 +4,18 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-
+/// <summary>
+/// I used this so everything could have a reference back to BoardManager, when I was still putting everything together.
+/// So the first-pass approach was: BMs know about everything, everything knows about BM, the logic is in BM. This is obviously NOT IDEAL
+/// The model I'm moving towards is:
+/// - State is kept in entity objects like Character, RecipeSituation and SituationElementContainer
+/// - When entity objects are created, UI elements (and other entities that need to know about changes) are added as subscribers
+/// - state changes are published and the UI updates
+/// 
+/// At the moment, the BoardManager tends to be a subscriber itself- I would rather make lower-level UI components subscribers
+/// Ultimately, I would expect to make BoardManager an explicit reference in the Inspector in all lower-level components, and
+/// retire this class completely.
+/// </summary>
     public class BoardMonoBehaviour: MonoBehaviour
     {
         private static BoardManager board;
