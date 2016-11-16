fb931629c8fe5f15afcaeab9b1a7add69cdaa85c
alexiskennedy <alexis@weatherfactory.biz>
2016-11-16 22:52:16 +0000

    Commented out inconclusive sample tests

--- Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
+++ Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
@@ -88,15 +88,15 @@ namespace UnityTest
         [Datapoint]
         public double infinity = double.PositiveInfinity;
 
-        [Theory]
-        public void SquareRootDefinition(double num)
-        {
-            Assume.That(num >= 0.0 && num < double.MaxValue);
+        //[Theory]
+        //public void SquareRootDefinition(double num)
+        //{
+        //    Assume.That(num >= 0.0 && num < double.MaxValue);
 
-            var sqrt = Math.Sqrt(num);
+        //    var sqrt = Math.Sqrt(num);
 
-            Assert.That(sqrt >= 0.0);
-            Assert.That(sqrt * sqrt, Is.EqualTo(num).Within(0.000001));
-        }
+        //    Assert.That(sqrt >= 0.0);
+        //    Assert.That(sqrt * sqrt, Is.EqualTo(num).Within(0.000001));
+        //}
     }
 }
