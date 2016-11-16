d24267923f7f15885bb20c76154812eb1ae347cd
alexiskennedy <alexis@weatherfactory.biz>
2016-11-16 22:20:50 +0000

    Removed editor references

--- Assets/Core/Enums/ContentImporter.cs
+++ Assets/Core/Enums/ContentImporter.cs
@@ -5,7 +5,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Noon;
 using OrbCreationExtensions;
-using UnityEditor;
 using UnityEngine.Assertions;
 
 public class ContentImporter
--- Assets/TabletopUi/Scripts/Infrastructure/Heart.cs
+++ Assets/TabletopUi/Scripts/Infrastructure/Heart.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Assets.CS.TabletopUI;
-using UnityEditor;
 
 /// <summary>
 /// Top-level object
