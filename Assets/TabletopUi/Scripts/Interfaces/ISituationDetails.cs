65eb3f2899580189ab56e77c627255e1ebb44a44
alexiskennedy <alexis@weatherfactory.biz>
2016-11-26 12:49:27 +0000

    And files I missed

--- Assets/TabletopUi/Scripts/Interfaces/ISituationDetails.cs
+++ Assets/TabletopUi/Scripts/Interfaces/ISituationDetails.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Core;
+using Assets.Core.Interfaces;
+using Assets.CS.TabletopUI.Interfaces;
 
 namespace Assets.TabletopUi.Scripts.Interfaces
 {
@@ -9,5 +12,14 @@ namespace Assets.TabletopUi.Scripts.Interfaces
     {
         void Show();
         void Hide();
+        void DisplayOngoing();
+        void DisplayStarting();
+        void DisplayAspects(IAspectsDictionary forAspects);
+        void DisplayRecipe(Recipe r);
+        IEnumerable<IElementStack> GetStacksInStartingSlots();
+        AspectsDictionary GetAspectsFromSlottedElements();
+        void AddOutput(IEnumerable<IElementStack> stacks,INotification notification);
+        void DisplaySituation(string stitle, string sdescription, string nextRecipeDescription);
+        void Retire();
     }
 }
