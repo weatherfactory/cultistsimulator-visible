ab517cc0efdfb14a957a2df4a14ed922dd2cd0ae
Alexis Kennedy <ak@underspace.net>
2016-12-03 00:36:07 +0000

    Removed failing sample tests (again)

--- Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
+++ Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
@@ -10,41 +10,8 @@ namespace UnityTest
     [Category("Sample Tests")]
     internal class SampleTests
     {
-        [Test]
-        [Category("Failing Tests")]
-        public void ExceptionTest()
-        {
-            throw new Exception("Exception throwing test");
-        }
+ 
 
-        [Test]
-        [Ignore("Ignored test")]
-        public void IgnoredTest()
-        {
-            throw new Exception("Ignored this test");
-        }
-
-        [Test]
-        [MaxTime(100)]
-        [Category("Failing Tests")]
-        public void SlowTest()
-        {
-            Thread.Sleep(200);
-        }
-
-        [Test]
-        [Category("Failing Tests")]
-        public void FailingTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        [Category("Failing Tests")]
-        public void InconclusiveTest()
-        {
-            Assert.Inconclusive();
-        }
 
         [Test]
         public void PassingTest()
@@ -88,15 +55,6 @@ namespace UnityTest
         [Datapoint]
         public double infinity = double.PositiveInfinity;
 
-        [Theory]
-        public void SquareRootDefinition(double num)
-        {
-            Assume.That(num >= 0.0 && num < double.MaxValue);
 
-            var sqrt = Math.Sqrt(num);
-
-            Assert.That(sqrt >= 0.0);
-            Assert.That(sqrt * sqrt, Is.EqualTo(num).Within(0.000001));
-        }
     }
 }
