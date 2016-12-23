c3693e1ca21b18fc1221240af0d5bf7943d7897a
Martin Nerurkar <mnerurkar@sharkbombs.com>
2016-12-23 17:17:34 +0100

    Splash Screen

---
+++ Assets/TabletopUi/Scripts/UI/SplashScreen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using Assets.CS.TabletopUI;
+
+public class SplashScreen : MonoBehaviour, IPointerClickHandler {
+
+	bool isLoading = false;
+	public string gameSceneName = "TabletopPrototype";
+	[SerializeField] CanvasGroupFader fader;
+
+	void Start() {
+		fader.Hide();
+	}
+
+	public void OnPointerClick (PointerEventData eventData) {
+		LoadGameScene();
+	}
+
+	void Update() {
+		if (Input.anyKeyDown)
+			LoadGameScene();
+	}
+
+	void LoadGameScene() {
+		if (isLoading)
+			return;
+
+		fader.Show();
+		isLoading = true;
+		Invoke("DoLoad", fader.durationTurnOn);
+	}
+
+	void DoLoad() {
+		SceneManager.LoadScene(gameSceneName);
+	}
+
+}
