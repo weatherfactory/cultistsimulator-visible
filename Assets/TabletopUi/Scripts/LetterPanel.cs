9ee29cd72c6c9ef8bd5d8f542debe826bbe5db8d
Alexis Kennedy <ak@underspace.net>
2017-01-11 15:52:03 +0000

    Letter panel

---
+++ Assets/TabletopUi/Scripts/LetterPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterPanel : MonoBehaviour {
+
+    public void Show()
+    {
+     gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+     gameObject.SetActive(false);
+
+    }
+}
