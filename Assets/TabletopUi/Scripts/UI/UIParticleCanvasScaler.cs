e5df195c317deee68ddfb493ab53192d6c5ef9b8
Martin Nerurkar <Democritus@gmx.de>
2017-01-17 11:32:11 +0100

    particle scaler, tweaked ashflakes

---
+++ Assets/TabletopUi/Scripts/UI/UIParticleCanvasScaler.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/* UI Particle Scaling script by Tobi Tobasco, questions: tobias@lostmountaingames.com, http://tobiasnoller.com/
+ * Just drag this script on any canvas which has child particle systems underneath. 
+ * Keep in mind, you should run this script when you change resolutions.
+ * Prewarmed particle system emitting on begin play, will probably not be affected by the script the first time they fire.
+ * Feel free to use this script in any project. 
+ * If you change/enhance this script, please share your optimizations with the community.
+*/
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Canvas))]
+public class UIParticleCanvasScaler : MonoBehaviour {
+
+    private Canvas canvas;
+    private ParticleSystem[] emitters;
+    private bool m_isInitialized;
+    private Vector3 m_canvasScale;
+
+    // Lifecycle
+
+    private void OnEnable()     { SetParticleScales(); }
+
+#if UNITY_EDITOR
+    private void Reset()        { SetParticleScales(); }
+    private void Update()       { SetParticleScales(); }
+#endif
+
+    // Trigger Update
+
+    private void SetParticleScales() {
+        Init();
+        ApplyScale();
+    }
+
+    // Initilaization
+
+    private void Init() {
+        if (!m_isInitialized) { 
+            canvas = GetComponent<Canvas>();
+            emitters = this.gameObject.GetComponentsInChildren<ParticleSystem>();
+            m_isInitialized = true;
+        }
+
+        m_canvasScale = GetCanvasScale();
+    }
+
+    // Set Particle Scale
+
+    private Vector3 GetCanvasScale() {
+        return new Vector3( canvas.scaleFactor * canvas.transform.localScale.x, 
+                            canvas.scaleFactor * canvas.transform.localScale.y, 
+                            canvas.scaleFactor * canvas.transform.localScale.z  );
+    }
+
+    private void ApplyScale() {
+        foreach (ParticleSystem p in emitters)
+            if (p != null)
+                p.transform.localScale = m_canvasScale; 
+    }
+
+}
