dcec637e96fa419a2df1cf97156b562570379cb9
Martin Nerurkar <Democritus@gmx.de>
2017-09-08 16:27:13 +0200

    added some comments

--- Assets/CanvasZoomTest.cs
+++ Assets/CanvasZoomTest.cs
@@ -6,13 +6,14 @@ public class CanvasZoomTest : MonoBehaviour {
 
     [Tooltip("This is the max window size we zoom out to, ensures that both sides are visible")]
     public Vector2 maxWindowSize = new Vector2(2400f, 1800f);
-    private float zoomScaleIn = 1f; // 1 == pixel perfect zoom
-    private float zoomScaleOut;     // Needs to be smalle than zoomScaleIn
+    private float zoomScaleIn = 1f; // 1 == 100% = pixel perfect zoom
+    private float zoomScaleOut;     // Needs to be smaller than zoomScaleIn to enable zooming
 
     public float zoomSpeed = 0.1f;
     public AnimationCurve zoomCurve; // evaluated to provide some easing across the whole zoom range
 
     // ranges from 0 to 1, with 0 zoomed in all the way, 1 zoomed out all the way
+    // Change these to adjust starting zoom
     private float currentZoom = 0.5f; 
     private float targetZoom = 0.5f;
     private const float zoomTolerance = 0.00001f; // snap when this close to target
@@ -28,7 +29,7 @@ public class CanvasZoomTest : MonoBehaviour {
             enabled = false; // Turn off to disable update
             return;
         }
-
+        
         canvas = GetComponent<Canvas>();
         SetScale(currentZoom);
 	}
@@ -53,6 +54,9 @@ public class CanvasZoomTest : MonoBehaviour {
         }
     }
 
+    // Here we get the currentZoom between 0 (zoomed in) and 1 (zoomed out)
+    // We use that to evaluate the curve to get another value between 0 and 1. This distorts the zoom so that zooming out is slower
+    // Then we use that value to get a scale factor between our min and max zoomScales and put that in the canvas
     void SetScale(float zoom) {
         canvas.scaleFactor = Mathf.Lerp(zoomScaleIn, zoomScaleOut, zoomCurve.Evaluate(zoom));
     }
