25e0b605e24dfa0092f818605c62bfb3260f293c
alexiskennedy <alexis@weatherfactory.biz>
2017-10-13 19:27:30 +0100

    Marimusic

---
+++ Assets/TabletopUi/Scripts/Audio/BackgroundMusic.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+
+    private IEnumerable<AudioClip> backgroundMusic;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] public int currentTrackNumber;
+    private System.Random random;
+	// Use this for initialization
+	void Awake ()
+	{
+	    backgroundMusic = ResourcesManager.GetBackgroundMusic();
+	    random=new System.Random();
+	}
+
+    void Start()
+    {
+        PlayNextClip();
+    }
+	
+	
+	void Update () {
+	    if (!audioSource.isPlaying)
+	    {
+	        PlayNextClip();
+	    }
+    }
+
+    public virtual void PlayNextClip()
+    {
+        //currentTrackNumber++;
+        //if (currentTrackNumber == backgroundMusic.Count())
+        //    currentTrackNumber = 0;
+        //PlayClip(currentTrackNumber);
+
+        PlayClip(random.Next(1, backgroundMusic.Count())); //never plays the first clip
+    }
+
+    public void PlayClip(int trackNumber)
+    {
+        audioSource.Stop();
+        var clip = backgroundMusic.ElementAt(trackNumber);
+        audioSource.PlayOneShot(clip);
+        Debug.Log(clip.name);
+    }
+    public  void PlayRandomClip()
+	    {
+        PlayClip(random.Next(0, backgroundMusic.Count()));
+        }
+
+    public void SetMute(bool mute)
+    {
+        audioSource.mute=mute;
+    }
+	
+}
---
+++ Assets/TabletopUi/Scripts/Audio/MenuScreenBackgroundMusic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MenuScreenBackgroundMusic : BackgroundMusic
+{
+
+
+
+    public override void PlayNextClip()
+    {
+        
+        PlayClip(currentTrackNumber);
+    }
+
+	
+}
