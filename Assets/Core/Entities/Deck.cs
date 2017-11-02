b172da2a832bad1967dee2035d80844019d6611b
alexiskennedy <alexis@weatherfactory.biz>
2017-11-02 17:02:40 +0000

    Fixed import tests; added Deck entity

---
+++ Assets/Core/Entities/Deck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Core.Interfaces;
+
+namespace Assets.Core.Entities
+{
+    /// <summary>
+    /// A Deck is a wrapped stack in the classic, not the Cultist Simulator-specific, sense.
+    /// I'm wrapping it partly to encapsulate implementation but also to minimise naming confusion.
+    /// </summary>
+    public class Deck
+    {
+        private Stack<IElementStack> cards;
+
+        public Deck()
+        {
+            cards=new Stack<IElementStack>();
+        }
+
+        public Deck(Stack<IElementStack> cards)
+        {
+            cards = new Stack<IElementStack>();
+        }
+
+        public IElementStack Draw()
+        {
+            return cards.Pop();
+        }
+
+        public void Add(IElementStack card)
+        {
+            cards.Push(card);
+        }
+    }
+}
