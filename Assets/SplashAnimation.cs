d4fa892a7d978f918f674e5b0e4c41d59ba1a480
alexiskennedy <alexis@weatherfactory.biz>
2017-11-06 12:45:23 +0000

    Added ident screen

---
+++ Assets/SplashAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class SplashAnimation : MonoBehaviour
+{
+    public VideoPlayer player;
+	// Use this for initialization
+	void Start ()
+	{
+	    player.loopPointReached += EndReached;
+	}
+	
+	void EndReached (VideoPlayer p) {
+		SceneManager.LoadScene(SceneNumber.QuoteScene);
+	}
+}
--- Assets/TabletopUi/Scripts/Infrastructure/SceneNumber.cs
+++ Assets/TabletopUi/Scripts/Infrastructure/SceneNumber.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public static class SceneNumber {
 
-    public const int SplashScene = 0;
-    public const int MenuScene = 1;
-    public const int GameScene = 2;
-    public const int EndScene = 3;
-    public const int NewGameScene = 4;
+    public const int LogoScene = 0;
+    public const int QuoteScene = 1;
+    public const int MenuScene = 2;
+    public const int GameScene = 3;
+    public const int EndScene = 4;
+    public const int NewGameScene = 5;
 
 }
