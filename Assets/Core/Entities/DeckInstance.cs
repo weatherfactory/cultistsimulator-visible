350639abe23901b63ef013234a78d4ea745826e4
alexiskennedy <alexis@weatherfactory.biz>
2017-12-08 20:44:26 +0000

    Deck default now works

---
+++ Assets/Core/Entities/DeckInstance.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Core.Entities
+{
+    public class DeckInstance : IDeckInstance
+    {
+        private IDeckSpec _deckSpec;
+        private Stack<string> _cards;
+
+        public string Id
+        {
+            get { return _deckSpec.Id; }
+        }
+
+        public DeckInstance(IDeckSpec spec)
+        {
+            if (spec == null)
+                throw new ApplicationException("Can't initialise a deckinstance with a null deckspec");
+
+            _deckSpec = spec;
+            _cards = new Stack<string>();
+
+        }
+
+        public void Reset()
+        {
+            var rnd = new Random();
+            var unshuffledStack = new Stack<string>();
+            foreach (var eId in _deckSpec.StartingCards)
+            {
+                unshuffledStack.Push(eId);
+            }
+
+            _cards = new Stack<string>(unshuffledStack.OrderBy(x => rnd.Next()));
+        }
+
+
+
+        public string Draw()
+        {
+            if (_cards.Any())
+                return _cards.Pop();
+            else
+                //if the deck is exhausted, always return the default card
+                return _deckSpec.DefaultCardId;
+        }
+
+
+        public void Add(string elementId)
+        {
+            _cards.Push(elementId);
+        }
+
+
+        public List<string> GetCurrentCardsAsList()
+        {
+            var cardsList = new List<string>(_cards);
+            cardsList.Reverse(); //it's a stack, so it goes from the top down
+            return cardsList;
+        }
+
+        public Hashtable GetSaveData()
+        {
+            var cardsHashtable = new Hashtable();
+            foreach (var c in GetCurrentCardsAsList())
+            {
+                var indexForTable = (cardsHashtable.Count + 1).ToString();
+                cardsHashtable.Add(indexForTable, c);
+            }
+            return cardsHashtable;
+        }
+    }
+}
--- Assets/Core/Entities/DeckSpec.cs
+++ Assets/Core/Entities/DeckSpec.cs
@@ -16,10 +16,10 @@ namespace Assets.Core.Entities
 
         string Id { get; }
         List<string> StartingCards { get; set; }
-
+        string DefaultCardId { get; set; }
     }
 
-    
+
     public interface IDeckInstance:ISaveable
     {
         /// <summary>
@@ -64,73 +64,5 @@ namespace Assets.Core.Entities
 
     }
 
-    public class DeckInstance:IDeckInstance
-    {
-        private IDeckSpec _deckSpec;
-        private Stack<string> _cards;
-
-        public string Id
-        {
-            get { return _deckSpec.Id; }
-        }
-
-        public DeckInstance(IDeckSpec spec)
-        {
-            if (spec == null)
-                throw new ApplicationException("Can't initialise a deckinstance with a null deckspec");
-
-            _deckSpec = spec;
-            _cards=new Stack<string>();
-            
-        }
-
-        public void Reset()
-        {
-            var rnd = new Random();
-            var unshuffledStack = new Stack<string>();
-            foreach (var eId in _deckSpec.StartingCards)
-            {
-                unshuffledStack.Push(eId);
-            }
-
-            _cards = new Stack<string>(unshuffledStack.OrderBy(x => rnd.Next()));
-        }
-
-
-
-        public string Draw()
-        {
-            if (_cards.Any())
-                return _cards.Pop();
-            else
-                return null;
-
-
-        }
-
-
-        public void Add(string elementId)
-        {
-            _cards.Push(elementId);
-        }
-
-
-        public List<string> GetCurrentCardsAsList()
-        {
-            var cardsList = new List<string>(_cards);
-            cardsList.Reverse(); //it's a stack, so it goes from the top down
-            return cardsList;
-        }
-
-        public Hashtable GetSaveData()
-        {
-            var cardsHashtable = new Hashtable();
-            foreach (var c in GetCurrentCardsAsList())
-            {
-                var indexForTable = (cardsHashtable.Count + 1).ToString();
-                cardsHashtable.Add(indexForTable, c);
-            }
-            return cardsHashtable;
-        }
-    }
+   
 }
