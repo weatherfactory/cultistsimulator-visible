96a16fb02e601620dc3b89e6cd83ce4b795d250f
alexiskennedy <alexis@weatherfactory.biz>
2017-12-15 17:44:46 +0000

    Typo; scratch

---
+++ Assets/Editor/BuildScratch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildScratch : MonoBehaviour {
+
+    [MenuItem("File/Test")]
+    public static void ScriptedOps()
+    {
+        
+    }
+}
