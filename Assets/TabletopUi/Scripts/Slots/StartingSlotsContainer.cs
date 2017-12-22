d64e1891a81a8dcaf799779da04413d7ed3f4d83
alexiskennedy <alexis@weatherfactory.biz>
2017-12-22 18:58:59 +0000

    StartingSlotsContainer is not longer an ITokenContainer

--- Assets/TabletopUi/Scripts/Slots/StartingSlotsContainer.cs
+++ Assets/TabletopUi/Scripts/Slots/StartingSlotsContainer.cs
@@ -14,7 +14,7 @@ using Assets.TabletopUi.Scripts;
 using Assets.TabletopUi.Scripts.Infrastructure;
 
 namespace Assets.TabletopUi.SlotsContainers {
-    public class StartingSlotsContainer : AbstractSlotsContainer, ITokenContainer {
+    public class StartingSlotsContainer : AbstractSlotsContainer {
 
         [SerializeField] SituationSlotManager slotManager;
         public CanvasGroupFader canvasGroupFader;
@@ -22,9 +22,6 @@ namespace Assets.TabletopUi.SlotsContainers {
         protected RecipeSlot primarySlot;
         private ElementStacksManager _stacksManager;
 
-        public bool AllowStackMerge {
-            get { return false; }
-        }
 
         public override void Initialise(SituationController sc) {
             base.Initialise(sc);
@@ -106,43 +103,6 @@ namespace Assets.TabletopUi.SlotsContainers {
 
         void ArrangeSlots() {
             slotManager.ReorderSlots();
-        }
-
-        public ElementStacksManager GetElementStacksManager() {
-            //In some places we've done it Initialise. Here, we're testing if it's null and then assigning on the fly
-            //This is because I'm going through and refactoring. Perhaps it should be consistent YOU TELL ME it's likely to get refactored further anyhoo
-            if (_stacksManager == null)
-            {
-                ITokenTransformWrapper tabletopStacksWrapper = new TokenTransformWrapper(transform);
-                _stacksManager = new ElementStacksManager(tabletopStacksWrapper,"startingslot");
-            }
-            return _stacksManager;
-        }
-
-
-        public void ElementStackRemovedFromContainer(ElementStackToken elementStackToken)
-        {
-            
-        }
-
-
-        public void TryMoveAsideFor(SituationToken potentialUsurper, DraggableToken incumbent, out bool incumbentMoved)
-        {
-            //I don't *think* this should ever be called. Let's find out.
-            //if it's not, ofc, we have one too few interfaces. The ITokenContainer is being used as both 'thing that has a stacksmanager' and 'direct parent that determines behaviour'
-            throw new NotImplementedException();
-        }
-
-        public void TryMoveAsideFor(ElementStackToken potentialUsurper, DraggableToken incumbent, out bool incumbentMoved)
-        {
-            //I don't *think* this should ever be called. Let's find out.
-            //if it's not, ofc, we have one too few interfaces. The ITokenContainer is being used as both 'thing that has a stacksmanager' and 'direct parent that determines behaviour'
-            throw new NotImplementedException();
-        }
-
-
-        public string GetSaveLocationInfoForDraggable(DraggableToken draggable) {
-            return (draggable.RectTransform.localPosition.x.ToString() + SaveConstants.SEPARATOR + draggable.RectTransform.localPosition.y).ToString();
         }
     }
 
