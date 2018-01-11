f67afbb6bbab46815a342930ef7752239889edcf
martin nerurkar <martin nerurkar>
2018-01-11 15:45:35 +0100

    DebugTools don't add to stack if card is Unique

--- Assets/TabletopUi/Scripts/DebugTools.cs
+++ Assets/TabletopUi/Scripts/DebugTools.cs
@@ -59,20 +59,23 @@ public class DebugTools : MonoBehaviour,IRollOverride
     void AddCard(string elementId)
     {
         var stackManager = tabletop.GetElementStacksManager();
-
         var existingStacks = stackManager.GetStacks();
 
+        var element = Registry.Retrieve<ICompendium>().GetElementById(elementId);
+
         //check if there's an existing stack of that type to increment
-        foreach (var stack in existingStacks)
-        {
-            if(stack.Id==elementId)
-            { 
-                stack.ModifyQuantity(1);
-                return;
+        if (!element.Unique) { 
+            foreach (var stack in existingStacks)
+            {
+                if(stack.Id==elementId)
+                { 
+                    stack.ModifyQuantity(1);
+                    return;
+                }
             }
         }
         //if we didn't jump out of loop with return, above
-       stackManager.ModifyElementQuantity(elementId,1, Source.Existing());
+        stackManager.ModifyElementQuantity(elementId,1, Source.Existing());
     }
 
     void DecrementElement(string elementId)
