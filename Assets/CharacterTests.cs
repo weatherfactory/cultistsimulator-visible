d4a77b9d411894a0037d9861dc440896dc555e14
alexiskennedy <alexis@weatherfactory.biz>
2018-01-17 10:58:41 +0000

    Testing tests

---
+++ Assets/CharacterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Assets.Editor.Tests
+{
+    [TestFixture]
+    public class TestTest
+    {
+        [SetUp]
+        public void Setup()
+        { }
+
+        [Test]
+        public void TestTestTest()
+        {
+            var r = 1;
+            Assert.AreEqual(1,r);
+
+        }
+
+
+    }
+}
