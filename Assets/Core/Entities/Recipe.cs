6ab1b7450234c6192d2084c7fd1a5148c91d5886
Martin Nerurkar <martin@playful.systems>
2018-02-23 18:28:59 +0100

    removed broken using

--- Assets/Core/Entities/Recipe.cs
+++ Assets/Core/Entities/Recipe.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Assets.Core;
-using NSubstitute.Routing.Handlers;
+//using NSubstitute.Routing.Handlers;
 
 public enum PortalEffect
 {
