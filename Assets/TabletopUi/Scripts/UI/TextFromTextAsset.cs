48cdff1039312d7e6adfae2a760ff05b763a52fd
Martin Nerurkar <martin@playful.systems>
2018-02-23 11:06:55 +0100

    pulling version news from text file

---
+++ Assets/TabletopUi/Scripts/UI/TextFromTextAsset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextFromTextAsset : MonoBehaviour {
+
+    public string textAssetName;
+    public TextMeshProUGUI textMesh;
+
+	void Start () {
+        var textAsset = Resources.Load<TextAsset>(textAssetName);
+
+        if (textAsset != null && textMesh != null)
+            textMesh.text = textAsset.text;
+	}
+	
+}
