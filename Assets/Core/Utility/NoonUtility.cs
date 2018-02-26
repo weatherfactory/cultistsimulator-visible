e2b76590076207f128946742301d821e39b30c39
alexiskennedy <alexis@weatherfactory.biz>
2018-02-26 18:48:24 +0000

    St Hydra fix

--- Assets/Core/Utility/NoonUtility.cs
+++ Assets/Core/Utility/NoonUtility.cs
@@ -62,7 +62,7 @@ namespace Noon
         public static bool UnitTestingMode { get; set; }
         public static int CurrentVerbosity =5;
 
-        public static VersionNumber VersionNumber = new VersionNumber("2018.2.c.1");
+        public static VersionNumber VersionNumber = new VersionNumber("2018.2.c.3");
 
         public static void Log(string message,int verbosityNeeded=0)
         {
