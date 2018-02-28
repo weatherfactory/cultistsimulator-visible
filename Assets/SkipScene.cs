e0b692d45e6c67ca9beaaf25a337f06b8fef8500
alexiskennedy <alexis@weatherfactory.biz>
2018-02-28 23:49:17 +0000

    Alternate Unity build

---
+++ Assets/SkipScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkipScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    SceneManager.LoadScene(SceneNumber.QuoteScene);
+    }
+	
+
+}
