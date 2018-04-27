55fa5970d12e2e364e6840dd219e563a5f3770b5
alexiskennedy <alexis@weatherfactory.biz>
2018-04-27 17:12:06 +0100

    Temptation mechanic for detective

--- Assets/Core/Utility/NoonUtility.cs
+++ Assets/Core/Utility/NoonUtility.cs
@@ -82,7 +82,7 @@ namespace Noon
     public class NoonUtility
     {
         public static bool UnitTestingMode { get; set; }
-        public static int CurrentVerbosity =5;
+        public static int CurrentVerbosity =1;
 
         public static VersionNumber VersionNumber = new VersionNumber("2018.4.k.12");
 
