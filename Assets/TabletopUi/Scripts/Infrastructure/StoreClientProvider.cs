a0e3afd40b0f9e7c8405b332d4fe69296393b2af
alexiskennedy <alexis@weatherfactory.biz>
2018-05-15 15:07:30 +0100

    Linux compilation nonsense

--- Assets/TabletopUi/Scripts/Infrastructure/StoreClientProvider.cs
+++ Assets/TabletopUi/Scripts/Infrastructure/StoreClientProvider.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Galaxy.Api;
+
 using Noon;
 
 namespace Assets.TabletopUi.Scripts.Infrastructure
@@ -30,11 +30,15 @@ Gog=2
 
         public StoreClientProvider()
         {
-            IAuthListener authListener = new GogAuthListener();
-            
-            GalaxyInstance.ListenerRegistrar().Register(GalaxyTypeAwareListenerAuth.GetListenerType(), authListener);
+#if UNITY_STANDALONE_LINUX
+#else
+            Galaxy.Api.IAuthListener authListener = new GogAuthListener();
+
+            Galaxy.Api.GalaxyInstance.ListenerRegistrar().Register(Galaxy.Api.GalaxyTypeAwareListenerAuth.GetListenerType(), authListener);
 
             _initialised = true;
+
+#endif
         }
 
         public bool IsAvailable(StoreClient storeClient)
@@ -44,12 +48,15 @@ Gog=2
             if (storeClient == StoreClient.Steam && Facepunch.Steamworks.Client.Instance != null)
                 return true;
             if (storeClient == StoreClient.Gog)
-
-            if (GogGalaxyManager.Instance == null)
+#if UNITY_STANDALONE_LINUX
+                return false;
+#else
+               if (GogGalaxyManager.Instance == null)
                     return false;
-            if (GalaxyInstance.User().SignedIn())
+            if (Galaxy.Api.GalaxyInstance.User().SignedIn())
                 return true;
 
+#endif
             return false;
         }
 
@@ -82,126 +89,28 @@ Gog=2
                     NoonUtility.Log("Trying to set Steam achievement " + achievementId + ", but it doesn't exist", 10);
             }
 
+#if UNITY_STANDALONE_LINUX
+#else
             if (IsAvailable(StoreClient.Gog))
             {
-                var gogStats = GalaxyInstance.Stats();
 
-                IUserStatsAndAchievementsRetrieveListener statsRetrieveListener = new AchievementRequest(achievementId,setStatus,gogStats);
-                IStatsAndAchievementsStoreListener statsStoreListener = new GogStatsAndAchievementsStoreListener();
-                GalaxyInstance.ListenerRegistrar().Register(GalaxyTypeAwareListenerUserStatsAndAchievementsRetrieve.GetListenerType(), statsRetrieveListener);
-                GalaxyInstance.ListenerRegistrar().Register(GalaxyTypeAwareListenerStatsAndAchievementsStore.GetListenerType(), statsStoreListener);
+                var gogStats = Galaxy.Api.GalaxyInstance.Stats();
+
+                Galaxy.Api.IUserStatsAndAchievementsRetrieveListener statsRetrieveListener = new AchievementRequest(achievementId,setStatus,gogStats);
+                Galaxy.Api.IStatsAndAchievementsStoreListener statsStoreListener = new GogStatsAndAchievementsStoreListener();
+                Galaxy.Api.GalaxyInstance.ListenerRegistrar().Register(Galaxy.Api.GalaxyTypeAwareListenerUserStatsAndAchievementsRetrieve.GetListenerType(), statsRetrieveListener);
+                Galaxy.Api.GalaxyInstance.ListenerRegistrar().Register(Galaxy.Api.GalaxyTypeAwareListenerStatsAndAchievementsStore.GetListenerType(), statsStoreListener);
 
                 gogStats.RequestUserStatsAndAchievements(); //when the request completes, the callback will fire Execute on the AchievementRequest we attached above
 
             }
-
+#endif
         }
 
 
     }
 
 
-    public class GogAuthListener : IAuthListener
-    {
-        public override void OnAuthSuccess()
-        {
-            NoonUtility.Log("GOG Galaxy logged on?" + GalaxyInstance.User().IsLoggedOn(),10);
-            NoonUtility.Log("GOG Galaxy signed in? " + GalaxyInstance.User().SignedIn(),10);
-        }
-
-        public override void OnAuthFailure(FailureReason failureReason)
-        {
-            NoonUtility.Log("GOG Galaxy auth failed:" + failureReason,10);
-        }
-
-        public override void OnAuthLost()
-        {
-            NoonUtility.Log("Authentication lost");
-        }
-    }
-
-    public class AchievementRequest : IUserStatsAndAchievementsRetrieveListener
-
-    {
-        private string _forAchievementId;
-        private bool _setStatus;
-        private IStats _gogStats;
-
-        public AchievementRequest(string achievementId, bool setStatus,IStats gogStats)
-        {
-            if (string.IsNullOrEmpty(achievementId))
-                throw new ApplicationException("Whoa there, don't try to pass an empty achievementid to a GOG achievement request");
-            if(gogStats==null)
-                throw new ApplicationException("Whoa there, we need an actual stats object in our GOG achievement request");
-
-            _forAchievementId = achievementId;
-            _setStatus = setStatus;
-            _gogStats = gogStats;
-        }
-
-        private void Execute()
-        {
-            if(_setStatus)
-                _gogStats.SetAchievement(_forAchievementId);
-            else
-            _gogStats.ClearAchievement(_forAchievementId);
-            _gogStats.StoreStatsAndAchievements();
-
-            NoonUtility.Log("Set GOG achievement: " + _forAchievementId, 10);
-        }
-
-        public override void OnUserStatsAndAchievementsRetrieveSuccess(GalaxyID userID)
-        {
-            NoonUtility.Log("Retrieved achievements: about to set " + _forAchievementId + " as " + _setStatus,10);
-            Execute();
-        }
-
-        public override void OnUserStatsAndAchievementsRetrieveFailure(GalaxyID userID, FailureReason failureReason)
-        {
-            NoonUtility.Log("Couldn't retrieve achievements: " + failureReason, 10);
-        }
-
-    }
-
-    //not in use!
-    public class AchievementList: IUserStatsAndAchievementsRetrieveListener
-
-    {
-        private IStats _gogStats;
-
-
-        public AchievementList(IStats gogStats)
-        {
-            _gogStats = gogStats;
-        }
-
-
-        public override void OnUserStatsAndAchievementsRetrieveSuccess(GalaxyID userID)
-        {
-            NoonUtility.Log("Retrieved achievements: about to list them."); //or I would if there was something in the API for this
-            
-        }
-
-        public override void OnUserStatsAndAchievementsRetrieveFailure(GalaxyID userID, FailureReason failureReason)
-        {
-            NoonUtility.Log("Couldn't retrieve achievements: " + failureReason, 10);
-        }
-
-    }
-
-    public class GogStatsAndAchievementsStoreListener : IStatsAndAchievementsStoreListener
-    {
-        public override void OnUserStatsAndAchievementsStoreSuccess()
-        {
-            NoonUtility.Log("Stored achievements", 10);
-
-        }
-
-        public override void OnUserStatsAndAchievementsStoreFailure(FailureReason failureReason)
-        {
-            NoonUtility.Log("Couldn't store achievements: " + failureReason, 10);
-        }
-    }
     
 
 }
