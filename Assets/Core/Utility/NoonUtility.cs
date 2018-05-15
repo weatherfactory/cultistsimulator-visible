c5bfb5421e2c391d9f616eb26a2f8026bc36a2ab
alexiskennedy <alexis@weatherfactory.biz>
2018-05-15 17:46:15 +0100

    Painting progress

--- Assets/Core/Utility/NoonUtility.cs
+++ Assets/Core/Utility/NoonUtility.cs
@@ -93,7 +93,7 @@ namespace Noon
         public static bool UnitTestingMode { get; set; }
         
 
-        public static int CurrentVerbosity =0;
+        public static int CurrentVerbosity =1;
         public static uint CultistSimulatorSteamAppId = 718670;
         //public static string CultistSimulatorGOGProductId = "1456702644";
         //public static string CultistSimulatorGOGClientId = "50757209545787544";
