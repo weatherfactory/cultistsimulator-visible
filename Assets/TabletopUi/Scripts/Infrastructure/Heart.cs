b1fc44648c1d76a52a53efbc72efc673a1da39e3
alexiskennedy <alexis@weatherfactory.biz>
2018-05-24 17:31:05 +0100

    Removed commented code

--- Assets/TabletopUi/Scripts/Infrastructure/Heart.cs
+++ Assets/TabletopUi/Scripts/Infrastructure/Heart.cs
@@ -23,7 +23,7 @@ public class Heart : MonoBehaviour
     //do major housekeeping every n beats
     private const int HOUSEKEEPING_CYCLE_BEATS = 20; //usually, a second
 	// Autosave tracking is now done in TabletopManager.Update()
-    //private int AUTOSAVE_CYCLE_HOUSEKEEPINGS = 300; //usually, five minutes; number of housekeeping events that should pass before we autosave
+    
     
     private const string METHODNAME_BEAT="Beat"; //so we don't get a tiny daft typo with the Invoke
     private float usualInterval;
