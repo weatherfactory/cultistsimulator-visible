118cd66a8abf477f2a6433d0441f5778042480c0
alexiskennedy <alexis@weatherfactory.biz>
2018-06-05 16:29:22 +0100

    Steamworks error handling

--- Assets/SteamworksIntegration.cs
+++ Assets/SteamworksIntegration.cs
@@ -11,9 +11,17 @@ public class SteamworksIntegration : MonoBehaviour
     private uint SteamAppId=718670;
 	// Use this for initialization
 	void Start () {
-	    Facepunch.Steamworks.Config.ForUnity(Application.platform.ToString());
-        if(Client.Instance==null)
-            new Facepunch.Steamworks.Client(SteamAppId);
+
+	    try
+	    {
+	        Facepunch.Steamworks.Config.ForUnity(Application.platform.ToString());
+	        if (Client.Instance == null)
+	            new Facepunch.Steamworks.Client(SteamAppId);
+	    }
+	    catch
+	    {
+            NoonUtility.Log("Failed Steam integration startup, which is fine if you're not on Steam.",1);
+	    }
     }
 	
 	// Update is called once per frame
