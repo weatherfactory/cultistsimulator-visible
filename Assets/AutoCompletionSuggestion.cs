578325268f6a330f178e8a879eaad9ac05d53eab
Lyrositor <gagne.marc@gmail.com>
2018-11-23 12:09:57 +0100

    Add icons to autocompletion suggestions

---
+++ Assets/AutoCompletionSuggestion.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class AutoCompletionSuggestion : MonoBehaviour
+{
+	[SerializeField] private Text _text;
+	[SerializeField] private Image _icon;
+	[SerializeField] private Button _button;
+
+	public string GetText()
+	{
+		return _text.text;
+	}
+
+	public void SetText(string suggestedId)
+	{
+		_text.text = suggestedId;
+	}
+
+	public void SetIconForElement(Element element)
+	{
+		if (element == null)
+			return;
+
+		_icon.sprite = element.IsAspect ? ResourcesManager.GetSpriteForAspect(element.Icon)
+			: ResourcesManager.GetSpriteForElement(element.Icon);
+	}
+
+	public void AddClickListener(UnityAction call)
+	{
+		_button.onClick.AddListener(call);
+	}
+}
