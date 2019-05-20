00bea9f57f4cf34d6d289e0d514258df13afdbe9
Lyrositor <gagne.marc@gmail.com>
2019-05-20 10:50:12 +0200

    Ensure the card hover detail doesn't leave the edges of the screen

--- Assets/TabletopUi/Scripts/Elements/CardHoverDetail.cs
+++ Assets/TabletopUi/Scripts/Elements/CardHoverDetail.cs
@@ -5,6 +5,9 @@ namespace TabletopUi.Scripts.Elements
 {
     public class CardHoverDetail : ElementStackSimple, ICanvasRaycastFilter
     {
+        [SerializeField] 
+        private RectTransform statusBar;
+        
         public void Show()
         {
             UpdatePosition();
@@ -21,16 +24,35 @@ namespace TabletopUi.Scripts.Elements
             return false;
         }
 
-        public void UpdatePosition()
+        private void UpdatePosition()
         {
             var canvas = GetComponentInParent<Canvas>();
-            Vector2 position;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(
                 canvas.transform as RectTransform,
-                Input.mousePosition,
+                ClampMousePosition(canvas, Input.mousePosition),
                 canvas.worldCamera,
-                out position);
+                out var position);
+            
             transform.position = canvas.transform.TransformPoint(position);
+        }
+
+        private Vector2 ClampMousePosition(Canvas canvas, Vector2 position)
+        {
+            // Note: this code isn't entirely correct, but it does the job well
+            // enough. - MG
+            var scale = canvas.scaleFactor;
+            var rect = ((RectTransform) transform).rect;
+            var width = rect.width * scale;
+            var height = rect.height * scale;
+            return new Vector2(
+                Mathf.Clamp(
+                    position.x, 
+                    width,
+                    canvas.worldCamera.pixelWidth - width),
+                Mathf.Clamp(
+                    position.y, 
+                    height + statusBar.rect.height * scale,
+                    canvas.worldCamera.pixelHeight - height));
         }
     }
 }
