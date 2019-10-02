3886aa2d4bcb5498bd43aeb1d3a01a890c0f7d4c
Lyrositor <gagne.marc@gmail.com>
2019-10-02 15:08:47 +0200

    Add a script for adding localisation stubs to Russian

--- Assets/Editor/LocalisationTools.cs
+++ Assets/Editor/LocalisationTools.cs
@@ -1,19 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using Assets.CS.TabletopUI;
 using Assets.TabletopUi.Scripts.Infrastructure.Modding;
-using OrbCreationExtensions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Noon;
 using UnityEditor;
 using UnityEngine;
-using YamlDotNet.Serialization;
 
 namespace Assets.Editor
 {
     public static class LocalisationTools
     {
+        private static readonly string[] ContentTypes =
+        {
+            "decks", "elements", "endings", "legacies", "recipes", "verbs"
+        };
+        private static readonly string[] ValidFields =
+        {
+            "id",
+            "label",
+            "description",
+            "startdescription",
+            "drawmessages",
+            "slots",
+            "linked",
+            "alt",
+            "alternativerecipes"
+        };
+
         [MenuItem("Tools/Build Localisation File")]
         public static void BuildLocalisationFile()
         {
@@ -73,96 +92,150 @@ namespace Assets.Editor
             Debug.Log($"Localisation file saved at '{path}'");
         }
 
-        [MenuItem("Tools/Strip Localised Content")]
-        public static void StripLocalisedContent()
+        [MenuItem("Tools/Update Localisation Stubs")]
+        public static void AddLocalisationStubs()
         {
-#if MODS
-            new Registry().Register(new ModManager(false));
-#endif
-            var compendium = new Compendium();
-            var contentImporter = new ContentImporter();
-            contentImporter.PopulateCompendium(compendium);
-
-            var pathToOldContentFiles = Path.Combine(Application.streamingAssetsPath, "content", "core_ru");
-            var pathToNewContentFiles = pathToOldContentFiles + "_new";
-            if (!Directory.Exists(pathToNewContentFiles))
+            var sourceContentPath = Path.Combine(Application.streamingAssetsPath, "content", "core");
+            var destContentPath = Path.Combine(Application.streamingAssetsPath, "content", "core_ru");
+            if (!Directory.Exists(destContentPath))
             {
-                Directory.CreateDirectory(pathToNewContentFiles);
+                Directory.CreateDirectory(destContentPath);
             }
-            var contentTypes = new[] {"decks", "elements", "endings", "legacies", "recipes", "verbs"};
-            var validFields = new[] {"id", "label", "description", "startdescription", "drawmessages", "slots"};
-            foreach (var contentType in contentTypes)
+
+            foreach (var contentType in ContentTypes)
             {
-                Debug.Log($"Processing {contentType}");
-                var oldContentFolder = Path.Combine(pathToOldContentFiles, contentType);
-                var newContentFolder = Path.Combine(pathToNewContentFiles, contentType);
-                var oldContentFiles = Directory.GetFiles(oldContentFolder).Where(f => f.EndsWith(".json"));
-                if (!Directory.Exists(newContentFolder))
+                var sourcePath = Path.Combine(sourceContentPath, contentType);
+                var sourceFiles = Directory.GetFiles(sourcePath).Where(f => f.EndsWith(".json"));
+                var destPath = Path.Combine(destContentPath, contentType);
+                if (!Directory.Exists(destPath))
                 {
-                    Directory.CreateDirectory(newContentFolder);
+                    Directory.CreateDirectory(destPath);
                 }
-                foreach (var oldContentFile in oldContentFiles)
+
+                foreach (var sourceFile in sourceFiles)
                 {
-                    var fileName = Path.GetFileName(oldContentFile);
-                    var newContentFileName = Path.Combine(newContentFolder, fileName);
-                    Debug.Log($"\tProcessing {fileName}");
+                    var fileName = Path.GetFileName(sourceFile);
+                    NoonUtility.Log($"Processing {contentType}/{fileName}");
+                    var destFile = Path.Combine(destPath, fileName);
 
-                    var table = SimpleJsonImporter.Import(File.ReadAllText(oldContentFile));
-                    var items = table.GetArrayList(contentType);
-                    foreach (Hashtable item in items)
+                    var data = SimpleJsonImporter.Import(File.ReadAllText(sourceFile));
+
+                    // Copy any existing translations first
+                    if (File.Exists(destFile))
                     {
-                        var keys = new List<string>(item.Keys.Cast<string>());
-                        foreach (var key in keys)
+                        var translatedEntities =
+                            ((ArrayList) SimpleJsonImporter.Import(File.ReadAllText(destFile))[contentType])
+                            .Cast<Hashtable>()
+                            .ToDictionary(entity => (string) entity["id"]);
+                        foreach (Hashtable entity in (ArrayList) data[contentType])
                         {
-                            if (!validFields.Contains(key))
+                            var entityId = (string) entity["id"];
+                            if (!translatedEntities.ContainsKey(entityId))
                             {
-                                item.Remove(key);
+                                continue;
                             }
-                            else if (key == "slots")
-                            {
-                                var slotIdx = 0;
-                                List<SlotSpecification> referenceSlots;
-                                switch (contentType)
-                                {
-                                    case "elements":
-                                        referenceSlots = compendium.GetElementById(item["id"].ToString())
-                                            .ChildSlotSpecifications;
-                                        break;
-                                    case "recipes":
-                                        referenceSlots = compendium.GetRecipeById(item["id"].ToString())
-                                            .SlotSpecifications;
-                                        break;
-                                    case "verbs":
-                                        referenceSlots = new List<SlotSpecification>
-                                        {
-                                            compendium.GetVerbById(item["id"].ToString()).PrimarySlotSpecification
-                                        };
-                                        break;
-                                    default:
-                                        throw new Exception($"Unexpected content type '{contentType}'");
-                                }
 
-                                foreach (Hashtable slotSpec in item.GetArrayList(key))
-                                {
-                                    var trueSlotId = referenceSlots[slotIdx++].Id;
-                                    var slotLabel = slotSpec.GetString("label") ?? slotSpec.GetString("id");
-                                    var slotDescription = slotSpec.GetString("description");
-                                    slotSpec.Clear();
-                                    slotSpec["id"] = trueSlotId;
-                                    slotSpec["label"] = slotLabel;
-                                    if (slotDescription != null)
-                                    {
-                                        slotSpec["description"] = slotDescription;
-                                    }
-                                }
-                            }
+                            var translatedEntity = translatedEntities[entityId];
+                            CopyTranslatedFields(entity, translatedEntity);
                         }
                     }
 
-                    var serializer = new SerializerBuilder()
-                        .JsonCompatible()
-                        .Build();
-                    File.WriteAllText(newContentFileName, serializer.Serialize(table));
+                    data[contentType] = PruneArrayList((ArrayList) data[contentType]);
+                    WriteJsonToFile(destFile, data);
+                }
+            }
+        }
+
+        private static void CopyTranslatedFields(Hashtable original, Hashtable translated)
+        {
+            foreach (var property in translated.Keys.Cast<string>().ToList())
+            {
+                switch (property)
+                {
+                    case "label":
+                    case "description":
+                    case "startdescription":
+                        original[property] = translated[property];
+                        break;
+
+                    case "drawmessages":
+                        var drawMessages = (Hashtable) original[property];
+                        var translatedDrawMessages = (Hashtable) translated[property];
+                        foreach (var drawMessageId in drawMessages.Keys.Cast<string>().ToList()
+                            .Where(drawMessageId => translatedDrawMessages.ContainsKey(drawMessageId)))
+                        {
+                            drawMessages[drawMessageId] = translatedDrawMessages[drawMessageId];
+                        }
+                        break;
+
+                    case "slots":
+                    case "linked":
+                    case "alt":
+                    case "alternativerecipes":
+                        var subEntities = (ArrayList) original[property];
+                        var translatedSubEntities = (ArrayList) translated[property];
+                        for (var i = 0; i < subEntities.Count; i++)
+                        {
+                            var subEntity = (Hashtable) subEntities[i];
+                            var translatedSubEntity = (Hashtable) translatedSubEntities[i];
+                            CopyTranslatedFields(subEntity, translatedSubEntity);
+                        }
+                        break;
+                }
+            }
+        }
+
+        private static ArrayList PruneArrayList(IEnumerable source)
+        {
+            return new ArrayList(source.Cast<Hashtable>().Select(PruneHashtable).ToList());
+        }
+
+        private static Entity PruneHashtable(Hashtable source)
+        {
+            var newEntity = new Entity();
+            foreach (string property in source.Keys.Cast<string>().Where(p => ValidFields.Contains(p)).ToList())
+            {
+                switch (property)
+                {
+                    case "slots":
+                    case "linked":
+                    case "alt":
+                    case "alternativerecipes":
+                        var newList = PruneArrayList((ArrayList) source[property]);
+                        var onlyIds = newList.Cast<Entity>().All(entry => entry.IsIdOnly());
+
+                        if (!onlyIds)
+                        {
+                            SetEntityProperty(newEntity, property, newList);
+                        }
+                        break;
+                    default:
+                        SetEntityProperty(newEntity, property, source[property]);
+                        break;
+                }
+            }
+            return newEntity;
+        }
+
+        private static void SetEntityProperty(Entity entity, string name, object value)
+        {
+            typeof(Entity).GetProperty(name).SetValue(entity, value);
+        }
+
+        private static void WriteJsonToFile(string filePath, object data)
+        {
+            using (var file = File.OpenWrite(filePath))
+            {
+                using (var writer = new StreamWriter(file))
+                {
+                    using (var jsonWriter = new JsonTextWriter(writer))
+                    {
+                        jsonWriter.Formatting = Formatting.Indented;
+                        jsonWriter.Indentation = ' ';
+                        jsonWriter.Indentation = 4;
+                        JsonSerializer serializer = new JsonSerializer {NullValueHandling = NullValueHandling.Ignore};
+                        serializer.Serialize(jsonWriter, data);
+                    }
                 }
             }
         }
@@ -183,6 +256,50 @@ namespace Assets.Editor
                 {
                     Write(writer, text);
                 }
+            }
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        private class Entity
+        {
+            [JsonProperty(Order = 1)]
+            public string id { get; set; }
+
+            [JsonProperty(Order = 2)]
+            public string label { get; set; }
+
+            [JsonProperty(Order = 3)]
+            public string description { get; set; }
+
+            [JsonProperty(Order = 4)]
+            public string startdescription { get; set; }
+
+            [JsonProperty(Order = 5)]
+            public Hashtable drawmessages { get; set; }
+
+            [JsonProperty(Order = 6)]
+            public ArrayList slots { get; set; }
+
+            [JsonProperty(Order = 7)]
+            public ArrayList linked { get; set; }
+
+            [JsonProperty(Order = 8)]
+            public ArrayList alt { get; set; }
+
+            [JsonProperty(Order = 9)]
+            public ArrayList alternativerecipes { get; set; }
+
+            public bool IsIdOnly()
+            {
+                return
+                    label == null
+                    && description == null
+                    && startdescription == null
+                    && drawmessages == null
+                    && slots == null
+                    && linked == null
+                    && alt == null
+                    && alternativerecipes == null;
             }
         }
     }
