3205c1e8efcf21c9c75a1fe1e834a01c8afbca55
Lyrositor <gagne.marc@gmail.com>
2019-10-07 12:55:56 +0200

    Change the way slot requirements are displayed
    Aspects should now wrap if they overflow.

---
+++ Assets/TabletopUi/Scripts/Elements/AspectSetsDisplay.cs
@@ -0,0 +1,85 @@
+using System.Collections.Generic;
+using Assets.Core;
+using Assets.CS.TabletopUI;
+using Assets.TabletopUi.Scripts.Services;
+using UnityEngine;
+
+namespace TabletopUi.Scripts.Elements
+{
+    public class AspectSetsDisplay : MonoBehaviour
+    {
+        [SerializeField] private bool isWithinDetailsWindow;
+        [SerializeField] private bool hideIfEmpty;
+        [SerializeField] private bool hasBrightBackground;
+
+        [SerializeField] private Transform[] setPrefixes;
+        [SerializeField] private Transform aspectsDisplay;
+
+        private readonly List<IAspectsDictionary> _aspectSets = new List<IAspectsDictionary>();
+
+        public void AddAspectSet(int setIndex, IAspectsDictionary aspects)
+        {
+            var requiredExpansion = setIndex + 1 - _aspectSets.Count;
+            if (requiredExpansion > 0)
+            {
+                // Expand the number of sets as necessary
+                _aspectSets.AddRange(new IAspectsDictionary[requiredExpansion]);
+            }
+
+            _aspectSets[setIndex] = aspects;
+            RebuildAspectsDisplay();
+        }
+
+        public void Clear()
+        {
+            foreach (Transform child in aspectsDisplay.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            gameObject.SetActive(!hideIfEmpty);
+        }
+
+        private void RebuildAspectsDisplay()
+        {
+            Clear();
+
+            var numAspects = 0;
+            for (var i = 0; i < _aspectSets.Count; i++)
+            {
+                var aspectSet = _aspectSets[i];
+                if (aspectSet == null || aspectSet.Count == 0)
+                {
+                    continue;
+                }
+
+                if (setPrefixes[i] != null)
+                {
+                    Instantiate(setPrefixes[i], aspectsDisplay, false);
+                }
+                foreach (var k in aspectSet.Keys)
+                {
+                    AddAspectToDisplay(k, aspectSet[k]);
+                    numAspects++;
+                }
+            }
+            gameObject.SetActive(!hideIfEmpty || numAspects > 0);
+        }
+
+        private void AddAspectToDisplay(string aspectId, int quantity)
+        {
+            Element aspect = Registry.Retrieve<ICompendium>().GetElementById(aspectId);
+
+            if (aspect == null)
+                return; // We can't find the aspect? Well then don't add anything
+
+            if (aspect.IsHidden)
+                return; //...because it's hidden
+
+            ElementFrame newElementFrame = PrefabFactory.CreateLocally<ElementFrame>(aspectsDisplay);
+            newElementFrame.PopulateDisplay(aspect, quantity, null, hasBrightBackground);
+
+            if (isWithinDetailsWindow)
+                newElementFrame.SetAsDetailWindowChild();
+        }
+    }
+}
