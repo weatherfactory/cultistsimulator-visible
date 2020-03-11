2221f04175565166abd07e05cd05ee85689e9c39
alexiskennedy <alexis@weatherfactory.biz>
2020-03-11 10:15:26 +0000

    Amended product distributions for Humble and itch

--- Assets/TabletopUi/Scripts/Editor/BuildScripts/BuildUtility.cs
+++ Assets/TabletopUi/Scripts/Editor/BuildScripts/BuildUtility.cs
@@ -176,14 +176,14 @@ namespace Assets.Core.Utility
 
 
             BuildProduct vanillaEdition=new BuildProduct(env,Product.VANILLA,false);
-            BuildProduct perpetualDLC=new BuildProduct(env,Product.PERPETUAL,true);
+            BuildProduct perpetualAllDlc=new BuildProduct(env,Product.PERPETUAL_ALLDLC,true);
             BuildProduct DancerDLC=new BuildProduct(env,Product.DANCER,true);
             BuildProduct PriestDLC=new BuildProduct(env,Product.PRIEST,true);
             BuildProduct GhoulDLC=new BuildProduct(env,Product.GHOUL,true);
             //   BuildProduct ExileDLC=new BuildProduct(env,Product.EXILE,true);
 
             products.Add(vanillaEdition);
-            products.Add(perpetualDLC);
+            products.Add(perpetualAllDlc);
             products.Add(DancerDLC);
             products.Add(PriestDLC);
             products.Add(GhoulDLC);
@@ -208,11 +208,11 @@ namespace Assets.Core.Utility
 
 
             BuildProduct vanillaEdition=new BuildProduct(env,Product.VANILLA,false);
-            BuildProduct perpetualDLC=new BuildProduct(env,Product.PERPETUAL,true);
+            BuildProduct perpetualAllDlc=new BuildProduct(env,Product.PERPETUAL_ALLDLC,true);
             //   BuildProduct ExileDLC=new BuildProduct(env,Product.EXILE,true);
 
             products.Add(vanillaEdition);
-            products.Add(perpetualDLC);
+            products.Add(perpetualAllDlc);
             //   products.Add(ExileDLC);
 
             BuildStorefront buildStorefront=new BuildStorefront(Storefront.Itch,OSs,products);
