4afebee78eb11749f4fba54061a1f1a1d7e2bc4d
alexiskennedy <alexis@weatherfactory.biz>
2020-03-19 17:32:14 +0000

    Now setting target frame rate (a) for all OSs (b) low for graphics quality 1

--- Assets/TabletopUi/Scripts/Infrastructure/TabletopManager.cs
+++ Assets/TabletopUi/Scripts/Infrastructure/TabletopManager.cs
@@ -195,10 +195,12 @@ namespace Assets.CS.TabletopUI {
        	    #if UNITY_STANDALONE_OSX
             // Vsync doesn't seem to limit the FPS on the mac so well, so we set it to 0 and force a target framerate (setting it to 0 any other way doesn't work, has to be done in code, apparently in Start not Awake too) - FM
             QualitySettings.vSyncCount = 0;
-      	    Application.targetFrameRate = 60;
             #else
-            QualitySettings.vSyncCount = 1;	// Force VSync on in case user has tried to disable it. No benefit, just burns CPU - CP
-            #endif
+            QualitySettings.vSyncCount = 1; // Force VSync on in case user has tried to disable it. No benefit, just burns CPU - CP
+#endif
+
+            SetFrameRateForCurrentGraphicsLevel();
+
             _situationBuilder = new SituationBuilder(tableLevelTransform, windowLevelTransform, _heart);
             NoonUtility.Log("Setting up services",10);
             //register everything used gamewide
@@ -1022,11 +1024,17 @@ namespace Assets.CS.TabletopUI {
         public static void SetGraphicsLevel(int level)
         {
             QualitySettings.SetQualityLevel(level);
-            if (level == 1)
-                Application.targetFrameRate = 30; //ram down the frame rate for v low quality
-            else
-                Application.targetFrameRate = 60;
+            SetFrameRateForCurrentGraphicsLevel();
 
+        }
+
+        public static void SetFrameRateForCurrentGraphicsLevel()
+        {
+            int currentLevel = QualitySettings.GetQualityLevel();
+            if (currentLevel > 1)
+                Application.targetFrameRate = 60;
+            else
+                Application.targetFrameRate = 30; //ram down the frame rate for v low quality
         }
 
         public static bool GetAccessibleCards()
