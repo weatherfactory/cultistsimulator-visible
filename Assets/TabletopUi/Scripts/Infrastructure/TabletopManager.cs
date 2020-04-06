f76ab7f353e7f40426b1231f6b8d550286d8888e
alexiskennedy <alexis@weatherfactory.biz>
2020-04-06 22:07:30 +0100

    Lotsa

--- Assets/TabletopUi/Scripts/Infrastructure/TabletopManager.cs
+++ Assets/TabletopUi/Scripts/Infrastructure/TabletopManager.cs
@@ -430,7 +430,7 @@ namespace Assets.CS.TabletopUI {
             // - legacy - in case we're displaying unusual info
             // stacks catalogue - so it can subscribe for notifications re changes
             _elementOverview.Initialise(character.ActiveLegacy, elementStacksCatalogue,compendium);
-            tabletopBackground.ShowTabletopFor(character.ActiveLegacy);
+            //tabletopBackground.ShowTabletopFor(character.ActiveLegacy);
 
         }
 
@@ -667,7 +667,7 @@ namespace Assets.CS.TabletopUI {
             _speedController.SetPausedState(true, false, true);
 
             _elementOverview.Initialise(storage.ActiveLegacy, Registry.Retrieve<StackManagersCatalogue>(), compendium);
-            tabletopBackground.ShowTabletopFor(storage.ActiveLegacy);
+           // tabletopBackground.ShowTabletopFor(storage.ActiveLegacy);
 
         }
 
