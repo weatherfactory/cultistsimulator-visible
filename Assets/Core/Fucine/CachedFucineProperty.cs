8a68c0be9e347a5cdbdfc92fd4cdfcf8acd732c5
alexiskennedy <alexis@weatherfactory.biz>
2020-06-25 13:56:13 +0100

    Moved new classes to distinct files

---
+++ Assets/Core/Fucine/CachedFucineProperty.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace Assets.Core.Fucine
+{
+    public class CachedFucineProperty
+    {
+
+        public PropertyInfo PropertyInfo { get; set; }
+        public Fucine FucineAttribute { get; set; }
+        public string Name => PropertyInfo.Name;
+    }
+}
---
+++ Assets/Core/Fucine/TypeInfoCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assets.Core.Fucine
+{
+    public static class TypeInfoCache<T>
+    {
+        // ReSharper disable once StaticMemberInGenericType - ReSharper is concerned we might not realise that a distinct field is stored for each different type argument
+        private static readonly HashSet<CachedFucineProperty> FucinePropertiesForType = new HashSet<CachedFucineProperty>();
+
+
+        //private constructor - it's a static class
+        //when TypeInfoCache<T> is referenced, it'll use reflection to pull that information, first time only.
+        //downside is we can't have a runtime Type instance without going back to reflection, so each Entity class needs an explicit compile-time reference to its own type.
+        static TypeInfoCache()
+        {
+            var entityTypeProperties = typeof(T).GetProperties();
+
+            foreach (var thisProperty in entityTypeProperties)
+            {
+                if (Attribute.GetCustomAttribute(thisProperty, typeof(Fucine)) is Fucine fucineAttribute)
+                {
+                    CachedFucineProperty cachedProperty = new CachedFucineProperty { PropertyInfo = thisProperty, FucineAttribute = fucineAttribute };
+                    FucinePropertiesForType.Add(cachedProperty);
+                }
+            }
+
+        }
+
+        public static HashSet<CachedFucineProperty> GetCachedFucinePropertiesForType()
+        {
+            //This will return the cached results for the type referenced as a parameter when calling the static class - 
+            //so we don't need to specify it again here
+            return FucinePropertiesForType;
+        }
+    }
+}
