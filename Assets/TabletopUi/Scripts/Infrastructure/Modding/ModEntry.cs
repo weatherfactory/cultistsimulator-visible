717f2f889f89fb69b19700518a85337fabe92a0c
alexiskennedy <alexis@weatherfactory.biz>
2020-08-03 17:27:47 +0100

    Now cataloguing Steam workshop items

--- Assets/TabletopUi/Scripts/Infrastructure/Modding/ModEntry.cs
+++ Assets/TabletopUi/Scripts/Infrastructure/Modding/ModEntry.cs
@@ -21,7 +21,7 @@ namespace Assets.TabletopUi.Scripts.Infrastructure.Modding
 
         public Button uploadButton;
         public TextMeshProUGUI uploadText;
-        public Babelfish uploadToggleBabel;
+        public Babelfish uploadBabel;
 
         public Button activationToggleButton;
         public TextMeshProUGUI activationToggleText;
@@ -38,14 +38,35 @@ namespace Assets.TabletopUi.Scripts.Infrastructure.Modding
         public void Initialize(Mod mod)
         {
             _mod = mod;
-            title.text = mod.Name + " (" + mod.Version + ")";
-            description.text = mod.Description;
-            UpdateEnablementDisplay();
 
+            title.text = _mod.Name + " (" + mod.Version + ")";
+            description.text = _mod.Description;
+            UpdateEnablementDisplay();
 
             uploadButton.onClick.AddListener(UploadModToStorefront);
             activationToggleButton.onClick.AddListener(ToggleActivation);
-            
+
+
+            if (mod.ModInstallType == ModInstallType.Local)
+            {
+                LocalImage.gameObject.SetActive(true);
+                SteamImage.gameObject.SetActive(false);
+                uploadButton.gameObject.SetActive(true);
+            }
+            else if(mod.ModInstallType==ModInstallType.SteamWorkshop)
+            {
+                LocalImage.gameObject.SetActive(false);
+                SteamImage.gameObject.SetActive(true);
+                uploadButton.gameObject.SetActive(false);
+            }
+            else
+            {
+                LocalImage.gameObject.SetActive(false);
+                SteamImage.gameObject.SetActive(false);
+                uploadButton.gameObject.SetActive(false);
+                NoonUtility.Log($"Problematic install type for mod {_mod.Id} {_mod.Name} - {mod.ModInstallType}");
+            }
+
 
         }
 
@@ -61,12 +82,10 @@ namespace Assets.TabletopUi.Scripts.Infrastructure.Modding
 
         public void ToggleActivation()
         {
-
             var modManager = Registry.Retrieve<ModManager>();
             _mod.Enabled = !_mod.Enabled;
             modManager.SetModEnableState(_mod.Id, _mod.Enabled);
             UpdateEnablementDisplay();
-
         }
 
 
@@ -79,14 +98,9 @@ namespace Assets.TabletopUi.Scripts.Infrastructure.Modding
         {
           //  AsyncCallback callBack=new AsyncCallback(ModUploadComplete);
             var storefrontServicesProvider=Registry.Retrieve<StorefrontServicesProvider>();
-
             Action<ModUploadedArgs> modUploadedAction = ModUploaded;
             uploadText.text = "sec...";
             await storefrontServicesProvider.UploadModForCurrentStorefront(_mod,modUploadedAction);
-
-           
-          
-    
         }
 
 
