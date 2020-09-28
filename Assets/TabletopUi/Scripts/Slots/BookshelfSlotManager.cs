ed8796163a50cc41fa690eacedc9d290f4c8c18f
alexiskennedy <alexis@weatherfactory.biz>
2020-09-28 17:02:11 +0100

    Working more, but situation creation is borked

---
+++ Assets/TabletopUi/Scripts/Slots/BookshelfSlotManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Core.Interfaces;
+using Assets.CS.TabletopUI;
+
+namespace Assets.TabletopUi.Scripts.Slots
+{
+   public class BookshelfSlotManager: AbstractSlotsManager
+    {
+        public override void RespondToStackRemoved(IElementStack stack, Context context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RespondToStackAdded(RecipeSlot slot, IElementStack stack, Context context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
