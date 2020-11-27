40232f9bdf165b02d3b70bda8931f4bc68d7fc09
Alexis Kennedy <alexis@weatherfactory.biz>
2020-11-27 15:43:24 +0000

    Mouse simulator works!

---
+++ Assets/Scripts/Meta/MouseDownSimulator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+
+namespace Assets.Scripts.Meta
+{
+   public class MouseDownSimulator: OnScreenControl
+   {
+       public TextMeshProUGUI OnLabel;
+       public TextMeshProUGUI OffLabel;
+
+       public void Update()
+       {
+           if (Keyboard.current.leftBracketKey.wasPressedThisFrame)
+               SimulateMouseDown();
+       }
+
+        public void SimulateMouseDown()
+            {
+                SendValueToControl(1.0f);
+                OnLabel.fontStyle = FontStyles.Bold;
+                OffLabel.fontStyle = FontStyles.Normal;
+            }
+
+            [InputControl]
+            [SerializeField]
+            private string MouseDownControlPath;
+
+            protected override string controlPathInternal
+            {
+                get => MouseDownControlPath;
+                set => MouseDownControlPath = value;
+            }
+    }
+}
---
+++ Assets/Scripts/Meta/MouseUpSimulator.cs
@@ -0,0 +1,38 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.OnScreen;
+
+namespace Assets.Scripts.Meta
+{
+    public class MouseUpSimulator : OnScreenControl
+    {
+        public TextMeshProUGUI OnLabel;
+        public TextMeshProUGUI OffLabel;
+
+        public void Update()
+        {
+            if(Keyboard.current.rightBracketKey.wasPressedThisFrame)
+                SimulatedMouseUp();
+        }
+        public void SimulatedMouseUp()
+        {
+            SendValueToControl(0.0f);
+            OnLabel.fontStyle = FontStyles.Bold;
+            OffLabel.fontStyle = FontStyles.Normal;
+        }
+
+
+        [InputControl]
+        [SerializeField]
+        private string MouseDownControlPath;
+
+        protected override string controlPathInternal
+        {
+            get => MouseDownControlPath;
+            set => MouseDownControlPath = value;
+        }
+    }
+}
