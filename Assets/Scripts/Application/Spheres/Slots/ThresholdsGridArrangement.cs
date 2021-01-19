4f91b49c7746ad97cdf4d3bf51ac1ca29cc9693f
alexiskennedy <alexis@weatherfactory.biz>
2021-01-19 13:41:41 +0000

    Fixed single slot, but now the arrangement on complex slot is hinky

---
+++ Assets/Scripts/Application/Spheres/Slots/ThresholdsGridArrangement.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SecretHistories.UI
+{
+    public class ThresholdsGridArrangement: AbstractThresholdArrangement
+    {
+        [SerializeField] RectTransform rect;
+        [SerializeField] Text clickCatcher;
+        [SerializeField] private Vector2 expectedThresholdSize;
+        [SerializeField] private Vector2 spacing;
+        [SerializeField] private Vector2 margin;
+
+        [SerializeField] private float sizeTransitionDuration;
+        [SerializeField] private float slotMoveTransitionDuration;
+        int numPerRow = 1;
+        int numRows = 1;
+        float slotSpacing;
+        void OnEnable()
+        {
+            SetNumPerRow();
+            float height = GetHeightForSlotCount();
+            SetHeight(height, true);
+        }
+
+        //This is currently not in use, but I'm not sure if it might need to be used for reference again
+
+        //public void ReorderThresholds()
+        //{
+
+        //    int oldRowCount = numRows;
+
+        //    // Set dimension values
+        //    SetNumPerRow();
+
+        //    // Set height if our row count has changed
+        //    if (numRows != oldRowCount)
+        //    {
+        //        float targetHeight = GetHeightForSlotCount();
+
+        //        if (gameObject.activeInHierarchy)
+        //        {
+        //            StartCoroutine(AdjustHeight(targetHeight, sizeTransitionDuration));
+        //        }
+        //        else
+        //        {
+        //            SetHeight(targetHeight, true);
+        //        }
+        //    }
+
+        //    int thresholdIndex = 0;
+
+        //    foreach (var t in _thresholds.Keys)
+        //        t.viz.MoveToPosition(GetPositionForIndex(thresholdIndex), slotMoveTransitionDuration);
+
+
+        //}
+
+        public override void ArrangeThreshold(ThresholdSphere threshold, int index)
+        {
+            threshold.viz.rectTrans.SetParent(rect);
+            threshold.viz.rectTrans.localScale = Vector3.one;
+            threshold.viz.rectTrans.localPosition = Vector3.zero;
+            threshold.viz.rectTrans.localRotation = Quaternion.identity;
+            threshold.viz.rectTrans.anchorMin = new Vector2(0f, 1f);
+            threshold.viz.rectTrans.anchorMax = threshold.viz.rectTrans.anchorMin;
+            threshold.viz.SetPosition(GetPositionForIndex(index));
+
+            // do not animate if we're not visible - usually only for first slot being created in Initialise
+            if (gameObject.activeInHierarchy)
+                threshold.viz.TriggerShowAnim();
+        }
+
+        protected void SetNumPerRow()
+        {
+            // one extra spacing added to width to compensate for spacing added to n slots, not n-1 slots.
+            numPerRow = Mathf.Max(1, Mathf.FloorToInt((rect.rect.width - margin.x - margin.x + spacing.x) / (expectedThresholdSize.x + spacing.x)));
+            // numRows = Mathf.Max(1, Mathf.CeilToInt(_thresholds.Count / (float)numPerRow));
+            slotSpacing = (rect.rect.width - margin.x - margin.x) / numPerRow;
+        }
+
+        float GetHeightForSlotCount()
+        {
+            // remove extra spacing because we add a spacing with each row and only need n-1 spaces
+            return margin.y + margin.y + numRows * (expectedThresholdSize.y + spacing.y) - spacing.y;
+        }
+
+        Vector2 GetPositionForIndex(int i)
+        {
+            // ReSharper disable once PossibleLossOfFraction
+            int rowNumberForThisThreshold = Mathf.FloorToInt(i / numPerRow);
+            int overflowXModuloBasedOnIndexAndNumPerRow = i % numPerRow;
+
+
+            float xPos = overflowXModuloBasedOnIndexAndNumPerRow * slotSpacing + expectedThresholdSize.x * 0.5f;
+
+            float yPos = rowNumberForThisThreshold * (expectedThresholdSize.y + spacing.y) + expectedThresholdSize.y * 0.5f;
+
+            float xPosWithMargin = margin.x + xPos;
+            float yPosWithMargin = margin.y + yPos;
+
+            float yPosWithMarginDownwards = yPosWithMargin * -1f;
+
+            return new Vector2(xPosWithMargin,
+                yPosWithMarginDownwards);
+
+
+        }
+
+        IEnumerator AdjustHeight(float target, float duration)
+        {
+            float time = 0f;
+            float current = rect.rect.height;
+
+
+            while (time < duration)
+            {
+                SetHeight(Mathf.Lerp(current, target, time / duration)); // TODO: Add some nice easing?
+                time += Time.deltaTime;
+                yield return null;
+            }
+
+            SetHeight(target);
+        }
+
+        void SetHeight(float height, bool resetPos = false)
+        {
+            rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+
+            // we also want to reset the start pos
+            if (resetPos)
+                rect.anchoredPosition = Vector3.zero;
+        }
+
+
+    }
+}
--- Assets/Scripts/Application/Spheres/Slots/ThresholdsWrangler.cs
+++ Assets/Scripts/Application/Spheres/Slots/ThresholdsWrangler.cs
@@ -19,35 +19,17 @@ namespace SecretHistories.UI {
     public class ThresholdsWrangler : MonoBehaviour,ISphereEventSubscriber {
 
         public ThresholdSphere thresholdSpherePrefab;
+        [SerializeField] private AbstractThresholdArrangement thresholdArrangement;
 
-
-        [SerializeField] RectTransform rect;
-        [SerializeField] ScrollRect scrollRect;
-        [SerializeField] Text clickCatcher;
-        [SerializeField] Vector2 slotSize = new Vector2(80f, 120f);
-        [SerializeField] Vector2 spacing = new Vector2(20f, 20f);
-        [SerializeField] Vector2 margin = new Vector2(20f, 40f);
-
-        [SerializeField] float sizeTransitionDuration = 0.2f;
-        [SerializeField] float slotMoveTransitionDuration = 0.2f;
         public readonly OnSphereAddedEvent OnSphereAdded = new OnSphereAddedEvent();
         public readonly OnSphereRemovedEvent OnSphereRemoved = new OnSphereRemovedEvent();
 
         private Dictionary<ThresholdSphere, FucinePath> _thresholds =
           new Dictionary<ThresholdSphere, FucinePath>();
 
-        int numPerRow = 1;
-        int numRows = 1;
-		float slotSpacing;
-      //  private SituationPath _situationPath;
         private IVerb _verb;
 
-        void OnEnable() {
-			SetNumPerRow();
-            float height = GetHeightForSlotCount();
-            SetHeight(height, true);
-            ToggleScrollOnSize(height);
-        }
+
 
         /// <summary>
         /// Removes any existing thresholds, so we only ever have one primary
@@ -66,8 +48,7 @@ namespace SecretHistories.UI {
 
         }
 
-
-
+        
         public void RemoveAllThresholds()
         {
             var thresholdsToRetire = new List<ThresholdSphere>(_thresholds.Keys);
@@ -87,121 +68,25 @@ namespace SecretHistories.UI {
                 thresholdToRemove.Retire(SphereRetirementType.Graceful);
         }
 
-        public void ReorderThresholds() {
-
-            int oldRowCount = numRows;
-
-            // Set dimension values
-            SetNumPerRow();
-
-            // Set height if our row count has changed
-            if (numRows != oldRowCount) {
-                float targetHeight = GetHeightForSlotCount();
-
-                if (gameObject.activeInHierarchy) { 
-                    StartCoroutine(AdjustHeight(targetHeight, sizeTransitionDuration));
-                }
-                else { 
-                    SetHeight(targetHeight, true);
-                    ToggleScrollOnSize(targetHeight);
-                }
-            }
-
-            int thresholdIndex = 0;
-            
-            foreach(var t in _thresholds.Keys)
-                t.viz.MoveToPosition(GetPositionForIndex(thresholdIndex), slotMoveTransitionDuration);
-          
-
-        }
-
-        public void SetNumPerRow() {
-            // one extra spacing added to width to compensate for spacing added to n slots, not n-1 slots.
-			numPerRow = Mathf.Max(1, Mathf.FloorToInt((rect.rect.width - margin.x - margin.x + spacing.x) / (slotSize.x + spacing.x)));
-			numRows = Mathf.Max(1, Mathf.CeilToInt(_thresholds.Count / (float) numPerRow));
-			slotSpacing = (rect.rect.width - margin.x - margin.x) / numPerRow;
-        }
-
-        float GetHeightForSlotCount() {
-            // remove extra spacing because we add a spacing with each row and only need n-1 spaces
-            return margin.y + margin.y + numRows * (slotSize.y + spacing.y) - spacing.y;
-        }
-
-        Vector2 GetPositionForIndex(int i) {
-            int yPos = Mathf.FloorToInt(i / numPerRow);
-            int xPos = i % numPerRow;
-
-			return new Vector2(margin.x + xPos * slotSpacing + slotSize.x * 0.5f, 
-				( margin.y + yPos * (slotSize.y + spacing.y) + slotSize.y * 0.5f) * -1f); // TODO: spacing still missing in positioning
-			/*
-			// positioning exactly according to margins
-			return new Vector2(margin.x + xPos * (slotSize.x + spacing.x) + slotSize.x * 0.5f, 
-				( margin.y + yPos * (slotSize.y + spacing.y) + slotSize.y * 0.5f) * -1f); // TODO: spacing still missing in positioning
-			*/
-        }
-
-        IEnumerator AdjustHeight(float target, float duration) {
-            float time = 0f;
-            float current = rect.rect.height;
-
-            ToggleScrollOnSize(target, true); // If we're going to be scrollable at the end of this, turn it on now
-
-            while (time < duration) {
-                SetHeight(Mathf.Lerp(current, target, time / duration)); // TODO: Add some nice easing?
-				time += Time.deltaTime;
-				yield return null;
-            }
-
-            SetHeight(target);
-            ToggleScrollOnSize(target, false); // if we're not going to be scrollable at the end of this, turn it off now
-        }
-
-        void SetHeight(float height, bool resetPos = false) {
-            rect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
-
-            // we also want to reset the start pos
-            if (resetPos)
-                rect.anchoredPosition = Vector3.zero;
-        }
-
-        void ToggleScrollOnSize(float height, bool? onlyToggleTo = null) {
-            bool toggleTo = height > scrollRect.viewport.rect.height;
-
-            // If we only want to turn off or on, throw us out
-            if (onlyToggleTo != null && toggleTo != onlyToggleTo)
-                return;
-
-            scrollRect.vertical = toggleTo;
-            clickCatcher.enabled = toggleTo;
-        }
+       
 
         protected ThresholdSphere AddThreshold(SphereSpec sphereSpec,FucinePath parentPath)
         {
-            var threshold = GameObject.Instantiate(thresholdSpherePrefab, rect);
-
+            var newThreshold = GameObject.Instantiate(thresholdSpherePrefab);
+            _thresholds.Add(newThreshold, parentPath);
             SpherePath newThresholdPath = new SpherePath(parentPath, sphereSpec.Id);
-            threshold.Initialise(sphereSpec, newThresholdPath);
+            newThreshold.Initialise(sphereSpec, newThresholdPath);
 
-            threshold.viz.rectTrans.SetParent(rect);
-            threshold.viz.rectTrans.localScale = Vector3.one;
-            threshold.viz.rectTrans.localPosition = Vector3.zero;
-            threshold.viz.rectTrans.localRotation = Quaternion.identity;
-            threshold.viz.rectTrans.anchorMin = new Vector2(0f, 1f);
-            threshold.viz.rectTrans.anchorMax = threshold.viz.rectTrans.anchorMin;
-            threshold.viz.SetPosition(GetPositionForIndex(_thresholds.Keys.Count));
+            
 
+            OnSphereAdded.Invoke(newThreshold);
+            newThreshold.Subscribe(this);
 
-            _thresholds.Add(threshold, parentPath);
-            OnSphereAdded.Invoke(threshold);
+            thresholdArrangement.ArrangeThreshold(newThreshold, _thresholds.Keys.Count);
 
 
-            threshold.Subscribe(this);
 
-            // do not animate if we're not visible - usually only for first slot being created in Initialise
-            if (gameObject.activeInHierarchy)
-                threshold.viz.TriggerShowAnim();
-
-            return threshold;
+            return newThreshold;
         }
 
         protected void AddChildThresholdsForStack(ElementStack stack, FucinePath parentPath)
