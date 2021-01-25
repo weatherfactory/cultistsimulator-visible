b91a271c7bb449f0b70267abb2053bad5f96da51
alexiskennedy <alexis@weatherfactory.biz>
2021-01-25 16:12:57 +0000

    Specified some red tests

--- Assets/Tests/UnitTests/EncausteryTests.cs
+++ Assets/Tests/UnitTests/EncausteryTests.cs
@@ -18,27 +18,33 @@ namespace Assets.Tests.UnitTests
     { }
 
 
-
     [IsEncaustableClass(typeof(EncaustedCommandX))]
     public class EncaustableWithUnmarkedPropertyX: IEncaustable
     {
         [Encaust]
-        public int MarkedProperty { get; }
-        public int UnmarkedProperty { get; }
+        public int MarkedProperty { get; set; }
+        public int UnmarkedProperty { get; set; }
     }
 
     [IsEncaustableClass(typeof(EncaustedCommandX))]
     public class ValidEncaustableX : IEncaustable
     {
         [Encaust]
-        public int MarkedProperty { get; }
+        public int MarkedProperty { get; set; }
+        [Encaust]
+        public int OtherMarkedProperty { get; set; }
         [DontEncaust]
-        public int OtherMarkedProperty { get; }
+        public int MarkedAsDontEncaustProperty { get; set; }
         public int UnmarkedFIeldShouldBeOkay { get; set; }
     }
 
     public class EncaustedCommandX
     {
+        public int MarkedProperty { get; set; }
+        public int OtherMarkedProperty { get; set; }
+        public int MarkedAsDontEncaustProperty { get; set; }
+
+
 
     }
 
@@ -60,33 +66,42 @@ namespace Assets.Tests.UnitTests
         {
         var encaustery=new Encaustery();
         var nex = new NonEncaustableX();
-        Assert.Throws(typeof(ApplicationException), () => encaustery.EncaustTo<EncaustedCommandX>(nex));
+        Assert.Throws<ApplicationException>( () => encaustery.EncaustTo<EncaustedCommandX>(nex));
         }
 
         [Test]
         public void EncaustingThrowsException_WhenGenericArgumentDoesntMatch_ToTypeForAttribute()
         {
             var encaustery = new Encaustery();
-            var eupx = new ValidEncaustableX();
-            Assert.Throws(typeof(ApplicationException), () => encaustery.EncaustTo<SituationCreationCommand>(eupx));
+            var vx = new ValidEncaustableX();
+            Assert.Throws<ApplicationException>( () => encaustery.EncaustTo<SituationCreationCommand>(vx));
         }
 
 
         [Test]
         public void EncaustingThrowsError_WhenPropertyEncaustmentStatusIsUnmarked()
         {
-            Assert.AreEqual(1,0);
+            var encaustery=new Encaustery();
+            var eupx=new EncaustableWithUnmarkedPropertyX();
+            Assert.Throws<ApplicationException>(() => encaustery.EncaustTo<EncaustedCommandX>(eupx));
         }
         [Test]
         public void EncaustablePropertyValue_PopulatesMatchInIEncaustment()
         {
-            Assert.AreEqual(1, 0);
+         var encaustery=new Encaustery();
+         var vx = new ValidEncaustableX {MarkedProperty = 1, OtherMarkedProperty = 2};
+         var encaustedCommand= encaustery.EncaustTo<EncaustedCommandX>(vx);
+         Assert.AreEqual(1,encaustedCommand.MarkedProperty);
+         Assert.AreEqual(2,encaustedCommand.OtherMarkedProperty);
         }
 
         [Test]
         public void NonEncaustablePropertyValue_DoesntPopulateMatchInIEncaustment()
         {
-            Assert.AreEqual(1, 0);
+            var encaustery = new Encaustery();
+            var vx = new ValidEncaustableX { MarkedAsDontEncaustProperty = 3 };
+            var encaustedCommand = encaustery.EncaustTo<EncaustedCommandX>(vx);
+            Assert.AreEqual(0,encaustedCommand.MarkedAsDontEncaustProperty);
         }
 
         [Test]
