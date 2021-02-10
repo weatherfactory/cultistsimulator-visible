395382568f9fba441f6192ee49e1243c912ca1f0
alexiskennedy <alexis@weatherfactory.biz>
2021-02-10 15:03:25 +0000

    LIFTOFF

---
+++ Assets/Scripts/Application/Spheres/NotesSphere.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SecretHistories.Enums;
+using SecretHistories.Spheres;
+
+namespace Assets.Scripts.Application.Spheres
+{
+    public class NotesSphere: Sphere
+    {
+        public override SphereCategory SphereCategory => SphereCategory.Notes;
+        public virtual bool AllowStackMerge => false;
+        
+        }
+}
