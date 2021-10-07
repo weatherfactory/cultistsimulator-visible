e688966585b3134afb5f9fe1e38d6ca10e3f4951
alexiskennedy <alexis@weatherfactory.biz>
2021-10-07 20:13:32 +0100

    Fixed silly startup bug

--- Assets/Scripts/Application/Tokens/TravelItineraries/TokenInertItinerary.cs
+++ Assets/Scripts/Application/Tokens/TravelItineraries/TokenInertItinerary.cs
@@ -12,10 +12,12 @@ namespace SecretHistories.Assets.Scripts.Application.Tokens
     public class TokenInertItinerary: TokenItinerary
     {
         private Token _travellingToken;
-        public override string TokenName => _travellingToken.name;
+        private string _tokenName;
+        public override string TokenName => _tokenName;
 
         public override void Depart(Token tokenToSend, Context context)
         {
+            _tokenName = tokenToSend.name;
             _travellingToken = tokenToSend;
         }
 
