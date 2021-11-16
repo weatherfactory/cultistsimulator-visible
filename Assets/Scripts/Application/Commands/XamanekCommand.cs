15fb7a7afe33922f802eefe9d8b6b55fbf5392b3
alexiskennedy <alexis@weatherfactory.biz>
2021-11-16 16:08:29 +0000

    Paatch and Xam work

--- Assets/Scripts/Application/Commands/XamanekCommand.cs
+++ Assets/Scripts/Application/Commands/XamanekCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SecretHistories.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace SecretHistories.Assets.Scripts.Application.Commands
 {
-    public class XamanekCommand
+    public class XamanekCommand:IEncaustment
     {
+        public Dictionary<string>
     }
 }
