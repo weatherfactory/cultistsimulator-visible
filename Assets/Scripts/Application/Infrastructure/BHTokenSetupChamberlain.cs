e7903f204746b664f7fcff6b84a1e0d483ce8602
alexiskennedy <alexis@weatherfactory.biz>
2021-12-14 17:33:35 +0000

    Added trial BHTokenSetupChamberlain

--- Assets/Scripts/Application/Infrastructure/BHTokenSetupChamberlain.cs
+++ Assets/Scripts/Application/Infrastructure/BHTokenSetupChamberlain.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SecretHistories.Commands;
+using SecretHistories.Core;
 using SecretHistories.Entities;
+using SecretHistories.Fucine;
+using SecretHistories.UI;
 
 namespace SecretHistories.Assets.Scripts.Application.Infrastructure
 {
@@ -12,7 +15,29 @@ namespace SecretHistories.Assets.Scripts.Application.Infrastructure
     {
         public override List<TokenCreationCommand> GetTokenCreationCommandsToEnactLegacy(Legacy forLegacy)
         {
-            throw new NotImplementedException();
+            
+            FucinePath librarySpherePath = Watchman.Get<HornedAxe>().GetDefaultSpherePath();
+
+            var commands = new List<TokenCreationCommand>();
+
+           // SituationCreationCommand startingSituation = new SituationCreationCommand(forLegacy.StartingVerbId);
+           // var startingDestinationForVerb = new TokenLocation(startingTokenDistributionStrategy.GetNextTokenPositionAndIncrementCount(), tabletopSpherePath);
+
+       //     TokenCreationCommand startingTokenCommand = new TokenCreationCommand(startingSituation, startingTokenDistributionStrategy.AboveBoardStartingLocation()).WithDestination(startingDestinationForVerb, startingTokenDistributionStrategy.GetPlacementDelay());
+        //    commands.Add(startingTokenCommand);
+
+            AspectsDictionary startingElements = new AspectsDictionary();
+            startingElements.CombineAspects(forLegacy.Effects);  //note: we don't reset the chosen legacy. We assume it remains the same until someone dies again.
+
+            foreach (var e in startingElements)
+            {
+                var elementStackCreationCommand = new ElementStackCreationCommand(e.Key, e.Value);
+                
+                TokenCreationCommand startingStackCommand = new TokenCreationCommand(elementStackCreationCommand, TokenLocation.Default(librarySpherePath));
+                commands.Add(startingStackCommand);
+            }
+
+            return commands;
         }
     }
 }
