6fd9b58939060701f1b4849ae3d7a84fe21c8488
alexiskennedy <alexis@weatherfactory.biz>
2022-01-14 16:38:26 +0000

    Added some stub code for Fitments. Removed hhimages and hhcontent empty defunct folders

---
+++ Assets/Scripts/Application/Commands/FitmentCreationCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SecretHistories.Abstract;
+using SecretHistories.Commands.SituationCommands;
+using UnityEngine;
+
+namespace SecretHistories.Commands
+{
+    public class FitmentCreationCommand : ITokenPayloadCreationCommand,IEncaustment
+    {
+
+ 
+        public ITokenPayload Execute(Context context)
+        {
+            throw new NotImplementedException();
+            //ElementStack elementStack = null;
+
+            //try
+            //{
+            //    var compendium = Watchman.Get<Compendium>();
+            //    var element = compendium.GetEntityById<Element>(EntityId);
+
+            //    //If we deserialise a stack, we'll already know its ID. If we're creating it for the first time, we need to pick an ID
+            //    if (String.IsNullOrEmpty(Id))
+            //        Id = element.DefaultUniqueTokenId();
+            //    if (LifetimeRemaining == 0 && !Defunct)
+            //        LifetimeRemaining = element.Lifetime; //I wish I didn't have to use 0 for 'uninitialised' and '0 remaining.'
+
+
+
+            //    var timeshadow = new Timeshadow(element.Lifetime, LifetimeRemaining, element.Resaturate);
+
+            //    elementStack = new ElementStack(Id, element, Quantity, timeshadow, context);
+
+            //    foreach (var m in Mutations)
+            //        elementStack.SetMutation(m.Key, m.Value, false);
+
+            //    foreach (var i in Illuminations)
+            //        elementStack.SetIllumination(i.Key, i.Value);
+
+            //}
+            //catch (Exception e)
+            //{
+
+            //    NoonUtility.Log("Couldn't create element with ID " + Id + " - " + e.Message + "(This might be an element that no longer exists being referenced in a save file?)");
+            //    elementStack?.Retire(RetirementVFX.None);
+            //}
+
+
+            //return elementStack;
+
+        }
+
+        public int Quantity { get; }
+        public List<PopulateDominionCommand> Dominions { get; set; }
+    }
+}
--- Assets/Scripts/Application/Spheres/RoomSphere.cs
+++ Assets/Scripts/Application/Spheres/RoomSphere.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SecretHistories.Commands;
 using SecretHistories.Enums;
 using SecretHistories.Fucine;
 using SecretHistories.Spheres;
 
 namespace SecretHistories.Assets.Scripts.Application.Spheres
-{ 
+{
+    [IsEmulousEncaustable(typeof(Sphere))]
+
     public class RoomSphere: Sphere
     {
         private Dictionary<string, FucinePath> _exits;
