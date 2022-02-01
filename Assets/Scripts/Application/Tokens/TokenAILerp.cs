98b9049769e2f47a637971b59dd76f84905f0bed
alexiskennedy <alexis@weatherfactory.biz>
2022-02-01 16:24:19 +0000

    Subclassed AILerp so I can hook into OnTargetReached

---
+++ Assets/Scripts/Application/Tokens/TokenAILerp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pathfinding;
+
+namespace SecretHistories.Assets.Scripts.Application.Tokens
+{
+    public class TokenAILerp: AILerp
+    {
+    }
+}
