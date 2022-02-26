9a4c33e12a4d2283e9042105ebd85260c73218ff
alexiskennedy <alexis@weatherfactory.biz>
2022-02-26 14:50:41 +0000

    Slot working on MortalWndow again

--- Assets/Scripts/Application/UI/Situation/MortalWindow.cs
+++ Assets/Scripts/Application/UI/Situation/MortalWindow.cs
@@ -1,52 +1,199 @@
-﻿using System;
+﻿#pragma warning disable 0649
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Numerics;
 using Assets.Scripts.Application.Infrastructure.Events;
+using Assets.Scripts.Application.UI.Situation;
 using SecretHistories.Abstract;
-using SecretHistories.Constants;
-using SecretHistories.Entities;
-using SecretHistories.Enums;
-using SecretHistories.Events;
-using SecretHistories.Fucine;
-using SecretHistories.Spheres;
-using SecretHistories.UI;
+using SecretHistories.Assets.Scripts.Application.Infrastructure.Events;
+using SecretHistories.Commands;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
+using SecretHistories.Entities;
+using SecretHistories.Fucine;
+using SecretHistories.Constants;
+using SecretHistories.Enums;
+using SecretHistories.Events;
+using SecretHistories.Spheres;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 namespace SecretHistories.UI
 {
+
+
+
+
     [RequireComponent(typeof(WindowPositioner))]
-    public class MortalWindow: MonoBehaviour,ISituationSubscriber,IPayloadWindow
+    public class MortalWindow : MonoBehaviour, ISituationSubscriber, IPayloadWindow
     {
-        private ITokenPayload _payload;
+
+
+        //[SerializeField] private StateEnum CurrentStateIdentifier;
+        //  [SerializeField] private string CurrentRecipeId;
+
+        [Header("Visuals")] [SerializeField] CanvasGroupFader canvasGroupFader;
+        public WindowPositioner positioner;
+
+        [Space] [SerializeField] Image artwork;
+        [SerializeField] TextMeshProUGUI title;
+        [SerializeField] private LayoutElement layoutElement;
+        [SerializeField] private float MinHeightBeforeExpansion;
+
+
+
+        [Space] [SerializeField] List<SituationDominion> Dominions;
+
+
+        [Space] [SerializeField] AspectsDisplay aspectsDisplay;
+
+        [SerializeField] Button startButton;
+        [SerializeField] TextMeshProUGUI startButtonText;
+
         public UnityEvent OnStart;
         public UnityEvent OnCollect;
         public UnityEvent OnWindowClosed;
         public OnSphereAddedEvent OnSphereAdded;
         public OnSphereRemovedEvent OnSphereRemoved;
 
-        [Header("Visuals")] [SerializeField] CanvasGroupFader canvasGroupFader;
-        public WindowPositioner positioner;
 
-        [Space] [SerializeField] Image _artwork;
-        [SerializeField] TextMeshProUGUI _title;
+        //  public TokenLocation LastOpenLocation;
 
-        [SerializeField] Button startButton;
-        [SerializeField] TextMeshProUGUI startButtonText;
+        private ITokenPayload _payload;
 
-        [Space] [SerializeField] List<SituationDominion> Dominions;
 
-        //TODO: make sure windowPositioner can call back here OK
-
-        public void SituationStateChanged(SecretHistories.Entities.Situation situation)
+        public bool IsVisible
         {
-            DisplayButtonState(situation);
+            get { return canvasGroupFader.IsFullyVisible() || canvasGroupFader.IsAppearing(); }
+        }
+
+        public string Title
+        {
+            get { return title.text; }
+            set { title.text = value; }
+        }
+
+        public Vector3 Position
+        {
+            get { return positioner.GetPosition(); }
+            set { positioner.SetPosition(value); }
+        }
+
+        public void Attach(ElementStack newElementStack)
+        {
+            throw new NotImplementedException("Can't attach a SituationWindow to an element stack");
+        }
+
+        public void Attach(Situation newSituation)
+        {
+
+            newSituation.AddSubscriber(this);
+
+            OnWindowClosed.AddListener(newSituation.Close);
+            OnStart.AddListener(newSituation.TryStart);
+            OnCollect.AddListener(newSituation.Conclude);
+            OnSphereAdded.AddListener(newSituation.AttachSphere);
+            OnSphereRemoved.AddListener(newSituation.DetachSphere);
+
+
+            startButton.onClick.AddListener(OnStart.Invoke);
+
+            foreach (var d in Dominions)
+                d.RegisterFor(newSituation);
+
+            startButton.gameObject.SetActive(true);
+
+
+            SituationStateChanged(newSituation);
+            TimerValuesChanged(newSituation);
+
+            _payload = newSituation;
+            _payload.OnChanged += OnPayloadChanged;
+
+            name = "Window_" + _payload.EntityId;
+            DisplayIcon(_payload.Icon);
+
+            Title = _payload.Label;
+
 
         }
+
+        private void OnPayloadChanged(TokenPayloadChangedArgs args)
+        {
+            Title = _payload.Label;
+
+            //This slightly obscurely named method is now where we open and close the window
+
+            if (args.ChangeType == PayloadChangeType.Retirement)
+                Retire();
+            if (args.ChangeType == PayloadChangeType.Opening && !this.IsVisible)
+                PayloadRequestsShow(args.Payload.GetRectTransform().position);
+
+            if (args.ChangeType == PayloadChangeType.Closing && this.IsVisible)
+                PayloadRequestsHide();
+
+
+
+        }
+
+
+        public void DisplayIcon(string icon)
+        {
+            Sprite sprite = ResourcesManager.GetSpriteForVerbLarge(icon);
+            artwork.sprite = sprite;
+        }
+
+        public void CloseButtonClicked()
+        {
+            OnWindowClosed.Invoke(); //calls down to payload, which should then call up again
+        }
+
+
+        public void PayloadRequestsShow(Vector3 startPosition)
+        {
+            if (!IsVisible)
+            {
+                SoundManager.PlaySfx("SituationWindowShow");
+                canvasGroupFader.Show();
+                positioner.Show(canvasGroupFader.durationTurnOn, startPosition); // Animates the window (position allows optional change in position)
+                NotifySpheresChanged(new Context(Context.ActionSource.SphereReferenceLocationChanged));
+            }
+
+
+        }
+
+        public void PayloadRequestsHide()
+        {
+            if (IsVisible)
+            {
+                SoundManager.PlaySfx("SituationWindowHide");
+                canvasGroupFader.Hide();
+                NotifySpheresChanged(new Context(Context.ActionSource.SphereReferenceLocationChanged));
+            }
+
+        }
+
+        public void Collect()
+        {
+            OnCollect.Invoke();
+        }
+
+
+
+
+
+        public void ContentsDisplayChanged(ContentsDisplayChangedArgs args)
+        {
+
+
+            layoutElement.minHeight = MinHeightBeforeExpansion + args.ExtraHeightRequested;
+        }
+
+
         void DisplayButtonState(Situation situation)
         {
 
@@ -67,118 +214,57 @@ namespace SecretHistories.UI
             }
 
         }
-        public void TimerValuesChanged(SecretHistories.Entities.Situation s)
+
+        // ACTIONS
+
+        public void SituationStateChanged(Situation situation)
+        {
+            DisplayButtonState(situation);
+            //   CurrentStateIdentifier = situation.StateIdentifier;
+            //    CurrentRecipeId = situation.RecipeId;
+        }
+
+        public void TimerValuesChanged(Situation situation)
         {
             //
         }
 
-        public void SituationSphereContentsUpdated(SecretHistories.Entities.Situation s)
+
+        public void SituationSphereContentsUpdated(Situation s)
         {
-            //
-        }
-
-        public string Title
-        {
-            get => _title.text;
-            set => _title.text = value;
-        }
-
-        public Vector3 Position
-        {
-            get => positioner.GetPosition();
-            set => positioner.SetPosition(value);
-        }
-
-        public bool IsVisible
-        {
-            get { return canvasGroupFader.IsFullyVisible() || canvasGroupFader.IsAppearing(); }
-        }
-
-        public void Attach(ElementStack elementStack)
-        {
-            throw new NotImplementedException("Can't attach a MortalWindow to an element stack");
-        }
-
-        public void Attach(SecretHistories.Entities.Situation newSituation)
-        {
-
-            newSituation.AddSubscriber(this);
-
-            OnWindowClosed.AddListener(newSituation.Close);
-            OnStart.AddListener(newSituation.TryStart);
-            OnCollect.AddListener(newSituation.Conclude);
-            OnSphereAdded.AddListener(newSituation.AttachSphere);
-            OnSphereRemoved.AddListener(newSituation.DetachSphere);
-
-foreach (var d in Dominions)
-                d.RegisterFor(newSituation);
-
-
-
-            SituationStateChanged(newSituation);
-            TimerValuesChanged(newSituation);
-
-            _payload = newSituation;
-            _payload.OnChanged += OnPayloadChanged;
-
-            name = "MortalWindow_" + _payload.EntityId;
-            DisplayIcon(_payload.Icon);
-
-            Title = _payload.Label;
-        }
-        public void DisplayIcon(string icon)
-        {
-            Sprite sprite = ResourcesManager.GetSpriteForVerbLarge(icon);
-            _artwork.sprite = sprite;
-        }
-        private void OnPayloadChanged(TokenPayloadChangedArgs args)
-        {
-            _title.text = _payload.Label;
-
-            //This slightly obscurely named method is now where we open and close the window
-
-            if (args.ChangeType == PayloadChangeType.Retirement)
-                Retire();
-            if (args.ChangeType == PayloadChangeType.Opening && !this.IsVisible)
-                PayloadRequestsShow(args.Payload.GetRectTransform().position);
-
-            if (args.ChangeType == PayloadChangeType.Closing && this.IsVisible)
-                PayloadRequestsHide();
-            
-        }
-
-        public void PayloadRequestsShow(Vector3 startPosition)
-        {
-            if (!IsVisible)
-            {
-                SoundManager.PlaySfx("SituationWindowShow");
-                canvasGroupFader.Show();
-                positioner.Show(canvasGroupFader.durationTurnOn, startPosition); // Animates the window (position allows optional change in position)
-                NotifySpheresChanged(new Context(Context.ActionSource.SphereReferenceLocationChanged));
-            }
-
-
-        }
-        public void CloseButtonClicked()
-        {
-            OnWindowClosed.Invoke(); //calls down to payload, which should then call up again
-        }
-        public void PayloadRequestsHide()
-        {
-            if (IsVisible)
-            {
-                SoundManager.PlaySfx("SituationWindowHide");
-                canvasGroupFader.Hide();
-                NotifySpheresChanged(new Context(Context.ActionSource.SphereReferenceLocationChanged));
-            }
+            //If we have a transient card changing lifetime - and perhaps in other circs - this can get called every heartbeat. Candidate for optimisation.   
+            var allAspectsToDisplay = s.GetAspects(false);
+            aspectsDisplay.DisplayAspects(allAspectsToDisplay);
+            DisplayButtonState(s);
 
         }
 
-        public void Collect()
+
+        //public override void MoveObject(PointerEventData eventData) {
+        //    Vector3 dragPos;
+        //    RectTransformUtility.ScreenPointToWorldPointInRectangle(Registry.Get<IDraggableHolder>().RectTransform, eventData.position, eventData.pressEventCamera, out dragPos);
+
+        //    // Potentially change this so it is using UI coords and the RectTransform?
+        //    rectTransform.position = new Vector3(dragPos.x + dragOffset.x, dragPos.y + dragOffset.y, dragPos.z + dragHeight);
+
+        //    // rotate object slightly based on pointer Delta
+        //    if (rotateOnDrag && eventData.delta.sqrMagnitude > 10f) {
+        //        // This needs some tweaking so that it feels more responsive, physica. Card rotates into the direction you swing it?
+        //        perlinRotationPoint += eventData.delta.sqrMagnitude * 0.001f;
+        //        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, -10 + Mathf.PerlinNoise(perlinRotationPoint, 0) * 20));
+        //    }
+        //}
+        public void Retire()
         {
-            OnCollect.Invoke();
+            _payload.OnChanged -= OnPayloadChanged;
+            Destroy(gameObject);
         }
 
+
+
+        /// <summary>
+        /// Something's happened that affects our spheres. Anything with an interest in its constituent spheres (like tokens travelling to 'em) will want to know.  It might be that this should go into the Situation.
+        /// </summary>
         public void NotifySpheresChanged(Context context)
         {
             List<Sphere> spheres = new List<Sphere>();
@@ -190,15 +276,12 @@ foreach (var d in Dominions)
             foreach (var sphere in spheres)
             {
                 SphereChangedArgs args;
+
+
                 args = new SphereChangedArgs(sphere, context);
                 sphere.NotifySphereChanged(args);
             }
 
-        }
-        public void Retire()
-        {
-            _payload.OnChanged -= OnPayloadChanged;
-            Destroy(gameObject);
         }
     }
 }
