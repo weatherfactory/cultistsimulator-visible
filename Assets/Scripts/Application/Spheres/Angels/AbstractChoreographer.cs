2d2b7534481a54c5a42f591ea86f6c0d70696f31
alexiskennedy <alexis@weatherfactory.biz>
2022-03-06 12:30:46 +0000

    Snapvalue 4 no longer treated as unknown value

--- Assets/Scripts/Application/Spheres/Angels/AbstractChoreographer.cs
+++ Assets/Scripts/Application/Spheres/Angels/AbstractChoreographer.cs
@@ -51,7 +51,7 @@ namespace SecretHistories.Spheres.Angels
               return 0.5f;
           else if (snapValue == 3f)
               return 0.25f;
-          else if (snapValue == 3f)
+          else if (snapValue == 4f)
               return 0.125f;
             else if (snapValue == 0f)
               return 0.125f; //set at smallest value
