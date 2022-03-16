9266552b50bf522bbd53947cde6fcc61c65967b7
alexiskennedy <alexis@weatherfactory.biz>
2022-03-16 13:54:10 +0000

    Added YOU HAVE MODS message to the UH O scene

--- Assets/Scripts/Application/InfoSceneController.cs
+++ Assets/Scripts/Application/InfoSceneController.cs
@@ -1,11 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using SecretHistories.Constants.Modding;
 using SecretHistories.UI;
 using UnityEngine;
 
 public class InfoSceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject modWarning;
+    public void Start()
+    {
+        var modManager = Watchman.Get<ModManager>();
+        if (modManager == null)
+            return;
+        try
+        {
+         
+            var enabledMods = modManager.GetEnabledModsLoadOrderList();
+        
+            if(enabledMods.Count>0)
+
+                modWarning.SetActive(true);
+            else
+                modWarning.SetActive(false);
+
+
+        }
+        catch (Exception e)
+        {
+            NoonUtility.Log($"Error when trying to establish modfulness in error scene: {e.ToString()}");
+        }
+
+    }
     public void BrowseToFiles()
     {
         OpenInFileBrowser.Open(Application.persistentDataPath);
