a54986772eae9cd000e35b732a14a98b6c91dd6c
alexiskennedy <alexis@weatherfactory.biz>
2022-04-06 12:22:34 +0100

    Book ghosts checkin

--- Assets/Scripts/Application/Tokens/Ghosts/BookGhost.cs
+++ Assets/Scripts/Application/Tokens/Ghosts/BookGhost.cs
@@ -32,28 +32,32 @@ namespace SecretHistories.Ghosts
         private GameObject spine;
         [SerializeField]
         private Image spineImage;
- 
 
-        public override void ShowAt(Sphere projectInSphere, Vector3 showAtAnchoredPosition3D, RectTransform tokenRectTransform)
-        {
+        [SerializeField]
+        private GameObject flat;
+        [SerializeField]
+        private Image flatImage;
 
-            if (projectInSphere == _projectedInSphere && rectTransform.anchoredPosition3D != showAtAnchoredPosition3D) //do a smooth transition if moving in the same projected sphere and position not already identical
-                AnimateGhostMovement(rectTransform.anchoredPosition3D, showAtAnchoredPosition3D);
-            else
-            {
-                rectTransform.SetParent(projectInSphere.GetRectTransform());
-                rectTransform.localScale = tokenRectTransform.localScale;
-                if (projectInSphere.SphereCategory == SphereCategory.World)
-                    Topple();
-                else
-                    StandUp();
+        //public override void ShowAt(Sphere projectInSphere, Vector3 showAtAnchoredPosition3D, RectTransform tokenRectTransform)
+        //{
+
+        //    if (projectInSphere == _projectedInSphere && rectTransform.anchoredPosition3D != showAtAnchoredPosition3D) //do a smooth transition if moving in the same projected sphere and position not already identical
+        //        AnimateGhostMovement(rectTransform.anchoredPosition3D, showAtAnchoredPosition3D);
+        //    else
+        //    {
+        //        rectTransform.SetParent(projectInSphere.GetRectTransform());
+        //        rectTransform.localScale = tokenRectTransform.localScale;
+        //        //if (projectInSphere.SphereCategory == SphereCategory.World)
+        //        //    Topple();
+        //        //else
+        //        //    StandUp();
 
 
-                rectTransform.anchoredPosition3D = showAtAnchoredPosition3D;
-                ShowCanvasGroupFader();
-                _projectedInSphere = projectInSphere;
-            }
-        }
+        //        rectTransform.anchoredPosition3D = showAtAnchoredPosition3D;
+        //        ShowCanvasGroupFader();
+        //        _projectedInSphere = projectInSphere;
+        //    }
+        //}
 
         private void StandUp()
         {
@@ -82,12 +86,28 @@ namespace SecretHistories.Ghosts
         {
             frontCover.SetActive(true);
             spine.SetActive(false);
+        //    UpdateGhostRect(frontCover);
         }
 
         public override void Understate()
         {
             frontCover.SetActive(false);
             spine.SetActive(true);
+           // UpdateGhostRect(spine);
+        }
+
+        private void UpdateGhostRect(GameObject toMatchObject)
+        {
+            RectTransform newSize = toMatchObject.GetComponent<RectTransform>();
+            if (newSize == null)
+            {
+                NoonUtility.LogWarning($"Trying to update recttransform for bookmanifestation from {toMatchObject.name} but it doesn't have a recttransform");
+                return;
+            }
+
+            var rt = gameObject.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(newSize.sizeDelta.x,
+                newSize.sizeDelta.y);
         }
 
         public override void UpdateVisuals(IManifestable manifestable)
