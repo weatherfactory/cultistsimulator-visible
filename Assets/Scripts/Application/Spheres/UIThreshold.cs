c66e2b6553539c529194d9619493135a20b8ca4d
alexiskennedy <alexis@weatherfactory.biz>
2022-04-20 17:19:34 +0100

    basic recipe stuff, wip

---
+++ Assets/Scripts/Application/Spheres/UIThreshold.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SecretHistories.Commands;
+using SecretHistories.Constants;
+using SecretHistories.Entities;
+using SecretHistories.Enums;
+using SecretHistories.Spheres;
+using SecretHistories.UI;
+using UnityEngine;
+
+namespace SecretHistories.Spheres
+{
+ [IsEmulousEncaustable(typeof(Sphere))]
+    public class UIThreshold: Sphere
+    {
+        public override SphereCategory SphereCategory => SphereCategory.Threshold;
+        public override bool AllowDrag
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override Vector3 TransformPositionInSphereToWorldSpace(Vector3 positionToTransform)
+        {
+            //This is a slight convenience, but also needs to be overridden for screen space spheres which we don't want to honour the z coord for - because it makes the 
+            //token get up in the camera's business and look briefly enormous
+
+            var UIPosition = Camera.main.ScreenToWorldPoint(positionToTransform);
+            //      position.z = (thisCanvas.transform.position - Camera.main.transform.position).magnitude;
+           
+      
+            //This is all pretty screwed. It needs a rethink, so we can actually drag the cards to the hand.
+
+
+            return UIPosition;
+        }
+
+    }
+}
